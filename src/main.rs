53c88e5522beea479801856469346daf3a175bc7
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-08-31 19:31:18 +0200

    first running version :-)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
+use anyhow::Result;
 use async_std::net::TcpStream;
-use tiberius::{AuthMethod, Client, Config};
+use tiberius::{AuthMethod, Client, Config, EncryptionLevel};
 
 #[async_std::main]
-async fn main() -> anyhow::Result<()> {
-    let mut config = Config::new();
-    config.host("8e835d.online-server.cloud");
-    config.port(1433);
-    config.authentication(AuthMethod::sql_server("SA", "Regatta4HD"));
-    println!("Configured.");
+async fn main() -> Result<()> {
+    let config = create_config();
 
     let tcp = TcpStream::connect(config.get_addr()).await?;
     tcp.set_nodelay(true)?;
-    println!("Created TcpStream.");
 
     let mut client = Client::connect(config, tcp).await?;
-    println!("Connected.");
 
     let stream = client.query("SELECT @P1", &[&1i32]).await?;
     let row = stream.into_row().await?.unwrap();
@@ -23,4 +18,14 @@ async fn main() -> anyhow::Result<()> {
     assert_eq!(Some(1), row.get(0));
 
     Ok(())
+}
+
+fn create_config() -> Config {
+    let mut config = Config::new();
+    config.host("8e835d.online-server.cloud");
+    config.port(1433);
+    config.authentication(AuthMethod::sql_server("SA", "Regatta4HD"));
+    config.database("Regatta_2022");
+    config.encryption(EncryptionLevel::NotSupported);
+    config
 }
