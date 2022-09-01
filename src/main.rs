a4b3b0a4dba314c2ba1cec552e94f4b081dbf17e
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-01 20:56:23 +0200

    improvements

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use anyhow::{Ok, Result};
 use async_std::net::TcpStream;
 use tiberius::{AuthMethod, Client, Config, EncryptionLevel, Row};
 
-const HEATS_QUERY: &str = "SELECT c.Comp_ID, c.Comp_Event_ID_FK, c.Comp_Number, c.Comp_RoundCode, c.Comp_Label, c.Comp_State, c.Comp_Cancelled, c.Comp_DateTime, o.Offer_RaceNumber, o.Offer_ShortLabel, o.Offer_LongLabel \
+const HEATS_QUERY: &str = "SELECT c.*, o.Offer_RaceNumber, o.Offer_ShortLabel, o.Offer_LongLabel \
     FROM Comp AS c \
     INNER JOIN Offer AS o ON o.Offer_ID = c.Comp_Race_ID_FK \
     WHERE c.Comp_Event_ID_FK = @P1";
+
+const HEAT_RESULT_QUERY: &str = "SELECT c.*, o.Offer_RaceNumber, o.Offer_ShortLabel, o.Offer_LongLabel \
+    FROM Comp c \
+    INNER JOIN Offer AS o ON o.Offer_ID = c.Comp_Race_ID_FK \
+    WHERE c.Comp_ID = @P1";
+
 const REGATTA_ID: i32 = 12;
 
 #[async_std::main]
@@ -14,19 +20,12 @@ async fn main() -> Result<()> {
 
     let mut client = create_client(config).await?;
 
-    println!("Query {HEATS_QUERY}");
+    let heats = get_heats(&mut client).await?;
+    println!("Heats count: {}", heats.len());
 
-    let rows = client
-        .query(HEATS_QUERY, &[&REGATTA_ID])
-        .await?
-        .into_first_result()
-        .await?;
-
-    let heats = get_heats(rows);
-    println!("Heats count: {}", heats.unwrap().len());
-
-    // println!("{:?}", row);
-    // assert_eq!(Some(1), row.get(0));
+    let heat_id = heats.first().unwrap().id;
+    let heat = get_heat_result(&mut client, heat_id).await?;
+    print_heat(&heat);
 
     Ok(())
 }
@@ -49,48 +48,78 @@ fn create_config() -> Config {
     config
 }
 
-fn get_heats(rows: Vec<Row>) -> Result<Vec<Heat>> {
+async fn get_heat_result(client: &mut Client<TcpStream>, heat_id: i32) -> Result<Heat> {
+    let rows = client
+        .query(HEAT_RESULT_QUERY, &[&heat_id])
+        .await?
+        .into_first_result()
+        .await?;
+
+    let row = rows.first().unwrap();
+
+    let heat = create_heat(row).unwrap();
+    Ok(heat)
+}
+
+async fn get_heats(client: &mut Client<TcpStream>) -> Result<Vec<Heat>> {
+    println!("Query {HEATS_QUERY}");
+
+    let rows = client
+        .query(HEATS_QUERY, &[&REGATTA_ID])
+        .await?
+        .into_first_result()
+        .await?;
+
     let mut heats: Vec<Heat> = Vec::new();
 
-    for row in rows {
-        let heat = Heat {
-            id: row.try_get("Comp_ID")?.unwrap_or_else(|| 0),
-            race_number: row
-                .try_get("Offer_RaceNumber")?
-                .unwrap_or_else(|| "")
-                .to_string(),
-            race_short_label: row
-                .try_get("Offer_ShortLabel")?
-                .unwrap_or_else(|| "")
-                .to_string(),
-            race_long_label: row
-                .try_get("Offer_LongLabel")?
-                .unwrap_or_else(|| "")
-                .to_string(),
-            number: row.try_get("Comp_Number")?.unwrap_or_else(|| 0),
-            round_code: row
-                .try_get("Comp_RoundCode")?
-                .unwrap_or_else(|| "")
-                .to_string(),
-            division_number: row.try_get("Comp_Label")?.unwrap_or_else(|| "").to_string(),
-            state: row.try_get("Comp_State")?.unwrap_or_else(|| 0),
-            cancelled: row.try_get("Comp_Cancelled")?.unwrap_or_else(|| false),
-        };
-        println!(
-            "Heat: id={}, race_number={}, number={}, round_code={}, division_number={}, race_short_label={}, state={}, cancelled={}, race_long_label={}", 
-            heat.id,
-            heat.race_number,
-            heat.number,
-            heat.round_code,
-            heat.division_number,
-            heat.race_short_label,
-            heat.state,
-            heat.cancelled,
-            heat.race_long_label
-        );
+    for row in &rows {
+        let heat = create_heat(row).unwrap();
+        print_heat(&heat);
         heats.push(heat);
     }
     Ok(heats)
+}
+
+fn create_heat(row: &Row) -> Result<Heat> {
+    let heat = Heat {
+        id: row.try_get("Comp_ID")?.unwrap_or_else(|| 0),
+        race_number: row
+            .try_get("Offer_RaceNumber")?
+            .unwrap_or_else(|| "")
+            .to_string(),
+        race_short_label: row
+            .try_get("Offer_ShortLabel")?
+            .unwrap_or_else(|| "")
+            .to_string(),
+        race_long_label: row
+            .try_get("Offer_LongLabel")?
+            .unwrap_or_else(|| "")
+            .to_string(),
+        number: row.try_get("Comp_Number")?.unwrap_or_else(|| 0),
+        round_code: row
+            .try_get("Comp_RoundCode")?
+            .unwrap_or_else(|| "")
+            .to_string(),
+        division_number: row.try_get("Comp_Label")?.unwrap_or_else(|| "").to_string(),
+        state: row.try_get("Comp_State")?.unwrap_or_else(|| 0),
+        cancelled: row.try_get("Comp_Cancelled")?.unwrap_or_else(|| false),
+    };
+    Ok(heat)
+}
+
+fn print_heat(heat: &Heat) {
+    println!(
+        "Heat: id={}, race_number={}, number={}, round_code={}, division_number={}, race_short_label={}, state={}, cancelled={}, race_long_label={}", 
+        heat.id,
+        heat.race_number,
+        heat.number,
+        heat.round_code,
+        heat.division_number,
+        heat.race_short_label,
+        heat.state,
+        heat.cancelled,
+        heat.race_long_label
+    );
 }
 
 struct Heat {
