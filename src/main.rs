a799b83d0606ca1930166ac947efff06642a2995
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-03 10:35:13 +0200

    added HeatRegistration

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ async fn main() -> Result<()> {
     let heats = aquarius_db::get_heats(&mut client).await?;
     println!("Heats count: {}", heats.len());
 
-    let heat_id = heats.first().unwrap().id;
-    let heat = aquarius_db::get_heat_result(&mut client, heat_id).await?;
-    aquarius_db::print_heat(&heat);
+    let heat_id = heats.get(1).unwrap().id;
+    let heat_registrations = aquarius_db::get_heat_registrations(&mut client, heat_id).await?;
+    println!("heat_registrations count: {}", heat_registrations.len());
 
     Ok(())
 }
