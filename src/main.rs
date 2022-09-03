6a46c871b047ca942ea2ce66d65adcdc87a173b0
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-03 21:39:35 +0200

    use bb8 pool

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod aquarius_db;
+mod connection_manager;
 mod rest_api;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
+use bb8::Pool;
+use connection_manager::{TiberiusConnectionManager, TiberiusPool};
 
 // #[async_std::main]
 // async fn main() -> Result<()> {
@@ -18,8 +21,14 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let manager: TiberiusConnectionManager =
+        TiberiusConnectionManager::new(aquarius_db::create_config()).unwrap();
+    let db_pool: TiberiusPool = Pool::builder().max_size(5).build(manager).await.unwrap();
+    let data: Data<TiberiusPool> = Data::new(db_pool);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(Data::clone(&data))
             .service(rest_api::hello)
             .service(rest_api::heats)
             .service(rest_api::heat_registrations)
--- src/rest_api.rs
+++ src/rest_api.rs
@@ -1,8 +1,11 @@
-use crate::aquarius_db::{
-    create_client, create_config, get_heat_registrations, get_heats, Heat, HeatRegistration,
+use crate::{
+    aquarius_db::{get_heat_registrations, get_heats, Heat, HeatRegistration},
+    connection_manager::TiberiusPool,
 };
+
 use actix_web::{
     get,
+    web::Data,
     web::{Json, Path},
     HttpResponse, Responder,
 };
@@ -13,18 +16,19 @@ async fn hello() -> impl Responder {
 }
 
 #[get("/api/heats")]
-async fn heats() -> Json<Vec<Heat>> {
-    let mut client = create_client(create_config()).await.unwrap();
+async fn heats(data: Data<TiberiusPool>) -> Json<Vec<Heat>> {
+    let mut client = data.get().await.unwrap();
+
     let heats = get_heats(&mut client).await.unwrap();
 
     Json(heats)
 }
 
 #[get("/api/heat_registrations/{id}")]
-async fn heat_registrations(path: Path<i32>) -> Json<Vec<HeatRegistration>> {
+async fn heat_registrations(path: Path<i32>, data: Data<TiberiusPool>) -> Json<Vec<HeatRegistration>> {
     let heat_id = path.into_inner();
+    let mut client = data.get().await.unwrap();
 
-    let mut client = create_client(create_config()).await.unwrap();
     let heat_registrations = get_heat_registrations(&mut client, heat_id).await.unwrap();
 
     Json(heat_registrations)
