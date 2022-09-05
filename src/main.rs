577801577138b7b22b69ff0b1152f0759f9ca1b6
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-05 16:51:40 +0200

    read environment variables

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ mod rest_api;
 use actix_web::{web::Data, App, HttpServer};
 use bb8::Pool;
 use connection_manager::TiberiusConnectionManager;
-use std::io::Result;
+use std::{env::var, io::Result};
 use tiberius::{AuthMethod, Config, EncryptionLevel};
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    let manager = TiberiusConnectionManager::new(create_config()).unwrap();
+    let config = create_config();
+    let manager = TiberiusConnectionManager::new(config).unwrap();
     let db_pool = Pool::builder().max_size(20).build(manager).await.unwrap();
     let data = Data::new(db_pool);
+    let http_port = get_http_port();
+
+    println!("Starting HTTP server on port {http_port}");
 
     HttpServer::new(move || {
         App::new()
@@ -22,18 +26,49 @@ async fn main() -> Result<()> {
             .service(rest_api::heats)
             .service(rest_api::heat_registrations)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", http_port))?
     .workers(4)
     .run()
     .await
 }
 
+fn get_http_port() -> u16 {
+    var("HTTP_PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .unwrap()
+}
+
+fn get_db_port() -> u16 {
+    var("DB_PORT")
+        .unwrap_or("1433".to_string())
+        .parse()
+        .unwrap()
+}
+
+fn get_db_host() -> String {
+    var("DB_HOST").unwrap_or("8e835d.online-server.cloud".to_string())
+}
+
+fn get_db_name() -> String {
+    var("DB_NAME").unwrap_or("Regatta_2022_Test".to_string())
+}
+
 fn create_config() -> Config {
+    let db_host = get_db_host();
+    let db_port = get_db_port();
+    let db_name = get_db_name();
+
+    println!(
+        "Database configuration: host={}, port={}, name={}",
+        db_host, db_port, db_name
+    );
+
     let mut config = Config::new();
-    config.host("8e835d.online-server.cloud");
-    config.port(1433);
+    config.host(db_host);
+    config.port(db_port);
+    config.database(db_name);
     config.authentication(AuthMethod::sql_server("SA", "Regatta4HD"));
-    config.database("Regatta_2022_Test");
     config.encryption(EncryptionLevel::NotSupported);
     config
 }
