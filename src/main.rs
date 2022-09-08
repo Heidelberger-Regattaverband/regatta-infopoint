2991bc3aa7f13def9d12f964b3e3bd90c06fc328
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-08 13:29:57 +0200

    cleanup and improvements

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@ use actix_web::{web::Data, App, HttpServer};
 use bb8::Pool;
 use connection_manager::TiberiusConnectionManager;
 use log::info;
-use std::{env::var, io::Result};
-use tiberius::{AuthMethod, Config, EncryptionLevel};
+use std::{env, io::Result};
+use tiberius::{AuthMethod, EncryptionLevel};
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     env_logger::init();
     info!("Starting infopoint");
 
-    let manager = TiberiusConnectionManager::new(create_config()).unwrap();
-    let pool = Pool::builder().max_size(20).build(manager).await.unwrap();
+    let pool: Pool<TiberiusConnectionManager> = create_pool().await;
     let data = Data::new(pool);
     let http_port = get_http_port();
 
@@ -38,37 +37,44 @@ async fn main() -> Result<()> {
     .await
 }
 
+async fn create_pool() -> Pool<TiberiusConnectionManager> {
+    let config = create_config();
+    let manager = TiberiusConnectionManager::new(config).unwrap();
+    let pool = Pool::builder().max_size(20).build(manager).await.unwrap();
+    pool
+}
+
 fn get_http_port() -> u16 {
-    var("HTTP_PORT")
+    env::var("HTTP_PORT")
         .unwrap_or("8080".to_string())
         .parse()
         .unwrap()
 }
 
 fn get_db_port() -> u16 {
-    var("DB_PORT")
+    env::var("DB_PORT")
         .unwrap_or("1433".to_string())
         .parse()
         .unwrap()
 }
 
 fn get_db_host() -> String {
-    var("DB_HOST").unwrap_or("8e835d.online-server.cloud".to_string())
+    env::var("DB_HOST").unwrap_or("8e835d.online-server.cloud".to_string())
 }
 
 fn get_db_name() -> String {
-    var("DB_NAME").unwrap_or("Regatta_2022_Test".to_string())
+    env::var("DB_NAME").unwrap_or("Regatta_2022_Test".to_string())
 }
 
 fn get_db_user() -> String {
-    var("DB_USER").unwrap_or("sa".to_string())
+    env::var("DB_USER").unwrap_or("sa".to_string())
 }
 
 fn get_db_password() -> String {
-    var("DB_PASSWORD").unwrap()
+    env::var("DB_PASSWORD").unwrap()
 }
 
-fn create_config() -> Config {
+fn create_config() -> tiberius::Config {
     let db_host = get_db_host();
     let db_port = get_db_port();
     let db_name = get_db_name();
@@ -79,7 +85,7 @@ fn create_config() -> Config {
         db_host, db_port, db_name, db_user
     );
 
-    let mut config = Config::new();
+    let mut config = tiberius::Config::new();
     config.host(db_host);
     config.port(db_port);
     config.database(db_name);
