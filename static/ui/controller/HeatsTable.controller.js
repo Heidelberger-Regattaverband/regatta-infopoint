4d963e63cbe540b5f790412ec9fd48c94a0158b6
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-14 21:59:43 +0200

    Added HeatDetails

--- static/ui/controller/HeatsTable.controller.js
+++ static/ui/controller/HeatsTable.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "../model/StateLabelFormatter",
   "../model/RaceLabelFormatter",
-  "../model/HeatLabelFormatter"
-], function (Controller, StateLabelFormatter, RaceLabelFormatter, HeatLabelFormatter) {
+  "../model/HeatLabelFormatter",
+  "sap/f/library"
+], function (Controller, StateLabelFormatter, RaceLabelFormatter, HeatLabelFormatter, fioriLibrary) {
   "use strict";
 
   return Controller.extend("de.regatta_hd.infopoint.controller.HeatsTable", {
@@ -17,7 +18,11 @@ sap.ui.define([
       var oSelectedItem = oEvent.getParameter("listItem");
       var oBindingCtx = oSelectedItem.getBindingContext("heat");
       var oItem = oBindingCtx.getModel().getProperty(oBindingCtx.getPath());
-      alert(JSON.stringify(oItem));
+      // alert(JSON.stringify(oItem));
+
+      var oFCL = this.getView().getParent().getParent();
+
+      oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
     }
 
   });
