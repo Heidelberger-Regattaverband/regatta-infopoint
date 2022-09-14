e9ef6aa51aeeffbe11b30896d4960761a2408417
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-14 11:40:15 +0200

    onSelectionChange event handler

--- static/ui/controller/HeatsTable.controller.js
+++ static/ui/controller/HeatsTable.controller.js
@@ -1,13 +1,19 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel",
   "../model/StateLabelFormatter",
   "../model/RaceLabelFormatter"
-], function (Controller, JSONModel, StateLabelFormatter, RaceLabelFormatter) {
+], function (Controller, StateLabelFormatter, RaceLabelFormatter) {
   "use strict";
 
   return Controller.extend("de.regatta_hd.infopoint.controller.HeatsTable", {
-    stateLabelFormatter: StateLabelFormatter
-  });
+    stateLabelFormatter: StateLabelFormatter,
 
+    onSelectionChange: function (oEvent) {
+      var oSelectedItem = oEvent.getParameter("listItem");
+      var sPath = oSelectedItem.getBindingContext("heat").getPath();
+      var oItem = this.getView().getModel("heat").getProperty(sPath);
+      alert(JSON.stringify(oItem));
+    }
+
+  });
 });
