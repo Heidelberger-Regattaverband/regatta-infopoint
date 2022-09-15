c6959c6d8fbd58905f51f0652ceabe7575de4418
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-15 15:14:32 +0200

    heat registration table

--- static/ui/controller/HeatsTable.controller.js
+++ static/ui/controller/HeatsTable.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
   "../model/StateLabelFormatter",
   "../model/RaceLabelFormatter",
   "../model/HeatLabelFormatter",
   "../model/DayFormatter",
   "../model/TimeFormatter",
   "sap/f/library"
-], function (Controller, StateLabelFormatter, RaceLabelFormatter, HeatLabelFormatter, DayFormatter, TimeFormatter, fioriLibrary) {
+], function (Controller, JSONModel, StateLabelFormatter, RaceLabelFormatter, HeatLabelFormatter, DayFormatter, TimeFormatter, fioriLibrary) {
   "use strict";
   return Controller.extend("de.regatta_hd.infopoint.controller.HeatsTable", {
 
@@ -27,9 +28,14 @@ sap.ui.define([
     onSelectionChange: function (oEvent) {
       var oSelectedItem = oEvent.getParameter("listItem");
       if (oSelectedItem) {
-        var oBindingCtx = oSelectedItem.getBindingContext("heat");
-        var oItem = oBindingCtx.getModel().getProperty(oBindingCtx.getPath());
+        let oBindingCtx = oSelectedItem.getBindingContext("heat");
+        let oItem = oBindingCtx.getModel().getProperty(oBindingCtx.getPath());
         // alert(JSON.stringify(oItem));
+
+        let oModel = new JSONModel();
+        oModel.loadData("/api/heats/" + oItem.id + "/registrations");
+
+        this.getOwnerComponent().setModel(oModel, "heatRegistration");
 
         var oFCL = this.getView().getParent().getParent();
         oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
