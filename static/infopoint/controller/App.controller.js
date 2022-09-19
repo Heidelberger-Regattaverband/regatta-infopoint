9cc562303ca59fdd3fa0af922ce70863d46adb78
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-19 16:28:08 +0200

    new application header and rem

--- static/infopoint/controller/App.controller.js
+++ static/infopoint/controller/App.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  "sap/ui/core/mvc/Controller",
+  "../model/Formatter"
+], function (Controller, Formatter) {
   "use strict";
   return Controller.extend("de.regatta_hd.infopoint.controller.App", {
+
+    formatter: Formatter,
 
     onInit: function () {
       this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
