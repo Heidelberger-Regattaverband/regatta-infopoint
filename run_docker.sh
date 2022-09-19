d44e7fbeae550aeb0f0e721e8e02ec39eb110cde
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-09-19 18:45:26 +0200

    title update

--- run_docker.sh
+++ run_docker.sh
@@ -2,8 +2,8 @@
 
 git pull -p
 
-docker stop infopoint
-
 docker build -t infopoint .
+
+docker stop infopoint
 
 docker run -d --rm --name infopoint -p 80:8080 --env DB_PASSWORD=${DB_PASSWORD} infopoint
