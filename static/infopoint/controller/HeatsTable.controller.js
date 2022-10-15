7ca0477eb9475fefec61e2a2e27874ff695aaff7
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-10-15 18:51:20 +0200

    next and previous navigation

--- static/infopoint/controller/HeatsTable.controller.js
+++ static/infopoint/controller/HeatsTable.controller.js
@@ -1,32 +1,28 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller",
+  "de/regatta_hd/infopoint/controller/Base.controller",
   "sap/ui/model/json/JSONModel",
   "sap/ui/model/Filter",
   'sap/ui/model/FilterOperator',
-  "../model/Formatter",
-  "sap/f/library"
-], function (Controller, JSONModel, Filter, FilterOperator, Formatter, fioriLibrary) {
+  "../model/Formatter"
+], function (BaseController, JSONModel, Filter, FilterOperator, Formatter) {
   "use strict";
-  return Controller.extend("de.regatta_hd.infopoint.controller.HeatsTable", {
+
+  return BaseController.extend("de.regatta_hd.infopoint.controller.HeatsTable", {
 
     formatter: Formatter,
 
     onInit: function () {
-      const oComponent = this.getOwnerComponent();
+      this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 
-      this.getView().addStyleClass(oComponent.getContentDensityClass());
+      this.getEventBus().subscribe("heat", "previous", this.previousHeat, this);
+      this.getEventBus().subscribe("heat", "next", this.nextHeat, this);
 
-      oComponent.getRouter().attachRouteMatched(function (oEvent) {
-        if (oEvent.getParameter("name") === "heats") {
-          const oRegatta = oComponent.getModel("regatta").getData();
-          this._loadHeatsModel(oRegatta);
-        }
+      this.getRouter().getRoute("heats").attachMatched(function () {
+        this.byId("heatsIconTabBar").setSelectedKey("all");
+        this._loadHeatsModel();
       }, this);
 
-      this.getOwnerComponent().getRouter().getRoute("heats").attachMatched(function () {
-        const oIconTabBar = this.byId("heatsIconTabBar");
-        oIconTabBar.setSelectedKey("all");
-      }, this);
+      this.heatsTable = this.getView().byId("heatsTable");
     },
 
     onSelectionChange: function (oEvent) {
@@ -34,17 +30,9 @@ sap.ui.define([
       if (oSelectedItem) {
         const oBindingCtx = oSelectedItem.getBindingContext("heats");
         const oHeat = oBindingCtx.getModel().getProperty(oBindingCtx.getPath());
-
-        const oModel = new JSONModel();
-        oModel.loadData("/api/heats/" + oHeat.id + "/registrations");
-
         this.getOwnerComponent().setModel(new JSONModel(oHeat), "heat");
-        this.getOwnerComponent().setModel(oModel, "heatRegistration");
 
-        this.getOwnerComponent().getRouter().navTo("heatRegistrations");
-
-        // const oFCL = this.getView().getParent().getParent();
-        // oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
+        this.getRouter().navTo("heatRegistrations");
       }
     },
 
@@ -68,15 +56,50 @@ sap.ui.define([
     },
 
     onNavBack: function () {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter.navTo("startpage", {}, true);
+      this.oHeatsModel = undefined;
+      this.navBack("startpage");
     },
 
-    _loadHeatsModel: function (oRegatta) {
-      const oHeatsModel = new JSONModel();
-      oHeatsModel.loadData("/api/regattas/" + oRegatta.id + "/heats");
+    previousHeat: function (channelId, eventId, parametersMap) {
+      const iIndex = this.heatsTable.indexOfItem(this.heatsTable.getSelectedItem());
+      const iPreviousIndex = iIndex > 1 ? iIndex - 1 : 0;
 
-      this.getOwnerComponent().setModel(oHeatsModel, "heats");
+      if (iIndex != iPreviousIndex) {
+        this.heatsTable.setSelectedItem(this.heatsTable.getItems()[iPreviousIndex]);
+        const oHeat = this.getViewModel("heats").getData()[iPreviousIndex];
+        this.getOwnerComponent().getModel("heat").setData(oHeat);
+      }
+    },
+
+    nextHeat: function (channelId, eventId, parametersMap) {
+      const aHeats = this.getViewModel("heats").getData();
+
+      this._fetchAll(aHeats);
+
+      const iIndex = this.heatsTable.indexOfItem(this.heatsTable.getSelectedItem());
+      const iNextIndex = iIndex < aHeats.length - 1 ? iIndex + 1 : iIndex;
+
+      if (iIndex != iNextIndex) {
+        this.heatsTable.setSelectedItem(this.heatsTable.getItems()[iNextIndex]);
+        this.getOwnerComponent().getModel("heat").setData(aHeats[iNextIndex]);
+      }
+    },
+
+    _loadHeatsModel: function () {
+      if (!this.oHeatsModel) {
+        const oRegatta = this.getOwnerComponent().getModel("regatta").getData();
+        this.oHeatsModel = new JSONModel();
+        this.oHeatsModel.loadData("/api/regattas/" + oRegatta.id + "/heats");
+        this.setViewModel(this.oHeatsModel, "heats");
+      }
+    },
+
+    _fetchAll: function (aHeats) {
+      const iActual = this.heatsTable.getGrowingInfo().actual;
+      if (aHeats.length > iActual) {
+        this.heatsTable.setGrowingThreshold(aHeats.length);
+        this.heatsTable.getBinding("items").filter([]);
+      }
     }
   });
 });
