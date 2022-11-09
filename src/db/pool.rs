1903f4ef9fcfb06bfc9169ac3ce91f313b924830
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-11-09 19:47:54 +0100

    cargo update

--- src/db/pool.rs
+++ src/db/pool.rs
@@ -11,10 +11,34 @@ pub struct TiberiusConnectionManager {
 }
 
 impl TiberiusConnectionManager {
-    /// Create a new `TiberiusConnectionManager`.
-    fn new() -> tiberius::Result<TiberiusConnectionManager> {
-        let config = create_config();
-        Ok(TiberiusConnectionManager { config })
+    /// Creates a new `TiberiusConnectionManager`.
+    fn new() -> TiberiusConnectionManager {
+        let config = Self::create_config();
+        TiberiusConnectionManager { config }
+    }
+
+    fn create_config() -> tiberius::Config {
+        let db_host = env::var("DB_HOST").expect("env variable `DB_HOST` should be set");
+        let db_port = env::var("DB_PORT")
+            .expect("env variable `DB_PORT` should be set")
+            .parse()
+            .unwrap();
+        let db_name = env::var("DB_NAME").expect("env variable `DB_NAME` should be set");
+        let db_user = env::var("DB_USER").expect("env variable `DB_USER` should be set");
+        let db_password =
+            env::var("DB_PASSWORD").expect("env variable `DB_PASSWORD` should be set");
+        info!(
+            "Database configuration: host={}, port={}, name={}, user={}",
+            db_host, db_port, db_name, db_user
+        );
+
+        let mut config = tiberius::Config::new();
+        config.host(db_host);
+        config.port(db_port);
+        config.database(db_name);
+        config.authentication(tiberius::AuthMethod::sql_server(db_user, db_password));
+        config.encryption(tiberius::EncryptionLevel::NotSupported);
+        config
     }
 }
 
@@ -24,7 +48,7 @@ impl bb8::ManageConnection for TiberiusConnectionManager {
     type Error = tiberius::error::Error;
 
     async fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        let tcp = TcpStream::connect(&self.config.get_addr()).await?;
+        let tcp = TcpStream::connect(self.config.get_addr()).await?;
         tcp.set_nodelay(true)?;
         tiberius::Client::connect(self.config.clone(), tcp).await
     }
@@ -41,33 +65,6 @@ impl bb8::ManageConnection for TiberiusConnectionManager {
 }
 
 pub async fn create_pool() -> Pool<TiberiusConnectionManager> {
-    let manager = TiberiusConnectionManager::new().unwrap();
+    let manager = TiberiusConnectionManager::new();
     Pool::builder().max_size(5).build(manager).await.unwrap()
-}
-
-fn get_db_password() -> String {
-    env::var("DB_PASSWORD").expect("env variable `DB_PASSWORD` should be set")
-}
-
-fn create_config() -> tiberius::Config {
-    let db_host = env::var("DB_HOST").expect("env variable `DB_HOST` should be set");
-    let db_port = env::var("DB_PORT")
-        .expect("env variable `DB_PORT` should be set")
-        .parse()
-        .unwrap();
-    let db_name = env::var("DB_NAME").expect("env variable `DB_NAME` should be set");
-    let db_user = env::var("DB_USER").expect("env variable `DB_USER` should be set");
-
-    info!(
-        "Database configuration: host={}, port={}, name={}, user={}",
-        db_host, db_port, db_name, db_user
-    );
-
-    let mut config = tiberius::Config::new();
-    config.host(db_host);
-    config.port(db_port);
-    config.database(db_name);
-    config.authentication(tiberius::AuthMethod::sql_server(db_user, get_db_password()));
-    config.encryption(tiberius::EncryptionLevel::NotSupported);
-    config
 }
