afe49eb091cb89c25c9ea0b2d0aae64cda248151
Markus Ofterdinger <markus@ofterdinger.de>
2022-11-19 12:32:55 +0100

    list

--- static/infoportal/controller/Statistics.controller.js
+++ static/infoportal/controller/Statistics.controller.js
@@ -16,10 +16,22 @@ sap.ui.define([
       this.navBack("startpage");
     },
 
-    _loadStatistics: function () {
+    _loadStatistics: async function () {
       const oStatisticsModel = new JSONModel();
-      oStatisticsModel.loadData("/api/regattas/" + this.getOwnerComponent().getRegattaId() + "/statistics");
-      this.getOwnerComponent().setModel(oStatisticsModel, "statistics");
+      await oStatisticsModel.loadData("/api/regattas/" + this.getOwnerComponent().getRegattaId() + "/statistics");
+
+      const oStatistics = oStatisticsModel.getData();
+      oStatistics.items = [];
+      oStatistics.items.push({ name: "Rennen gesamnt", value: oStatistics.races.all });
+      oStatistics.items.push({ name: "Rennen abgesagt", value: oStatistics.races.cancelled });
+      oStatistics.items.push({ name: "Laeufe gesamnt", value: oStatistics.heats.all });
+      oStatistics.items.push({ name: "Laeufe abgesagt", value: oStatistics.heats.cancelled });
+      oStatistics.items.push({ name: "Laeufe gestarted", value: oStatistics.heats.started });
+      oStatistics.items.push({ name: "Laeufe beendet", value: oStatistics.heats.finished });
+      oStatistics.items.push({ name: "Laeufe offizielle", value: oStatistics.heats.official });
+      oStatistics.items.push({ name: "Laeufe bevorstehend", value: oStatistics.heats.pending });
+
+      this.setViewModel(oStatisticsModel, "statistics");
     }
 
   });
