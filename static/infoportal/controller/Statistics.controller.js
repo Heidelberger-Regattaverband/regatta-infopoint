05cbb90aa176b2cc4a8ec155f57e581a3b7dccf4
Markus Ofterdinger <markus@ofterdinger.de>
2022-11-19 18:26:19 +0100

    clubs stats

--- static/infoportal/controller/Statistics.controller.js
+++ static/infoportal/controller/Statistics.controller.js
@@ -22,13 +22,14 @@ sap.ui.define([
 
       const oStatistics = oStatisticsModel.getData();
       oStatistics.items = { registrations: [], races: [], heats: [] };
-      oStatistics.items.registrations.push({ name: this.i18n("common.all"), value: oStatistics.registrations.all });
+      oStatistics.items.registrations.push({ name: this.i18n("common.overall"), value: oStatistics.registrations.all });
       oStatistics.items.registrations.push({ name: this.i18n("common.cancelled"), value: oStatistics.registrations.cancelled });
-      oStatistics.items.registrations.push({ name: this.i18n("common.ownerClubs"), value: oStatistics.registrations.clubs });
+      oStatistics.items.registrations.push({ name: this.i18n("statistics.reportingClubs"), value: oStatistics.registrations.registeringClubs });
+      oStatistics.items.registrations.push({ name: this.i18n("statistics.participatingClubs"), value: oStatistics.registrations.clubs });
       oStatistics.items.registrations.push({ name: this.i18n("common.athletes"), value: oStatistics.registrations.athletes });
-      oStatistics.items.races.push({ name: this.i18n("common.all"), value: oStatistics.races.all });
+      oStatistics.items.races.push({ name: this.i18n("common.overall"), value: oStatistics.races.all });
       oStatistics.items.races.push({ name: this.i18n("common.cancelled"), value: oStatistics.races.cancelled });
-      oStatistics.items.heats.push({ name: this.i18n("common.all"), value: oStatistics.heats.all });
+      oStatistics.items.heats.push({ name: this.i18n("common.overall"), value: oStatistics.heats.all });
       oStatistics.items.heats.push({ name: this.i18n("heat.state.official"), value: oStatistics.heats.official });
       oStatistics.items.heats.push({ name: this.i18n("heat.state.finished"), value: oStatistics.heats.finished });
       oStatistics.items.heats.push({ name: this.i18n("heat.state.started"), value: oStatistics.heats.started });
