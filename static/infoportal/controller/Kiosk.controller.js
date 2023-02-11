be2d877be6a9deff25d2cd5e25ba355c223bab00
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-02-11 12:08:57 +0100

    loading kiosk model

--- static/infoportal/controller/Kiosk.controller.js
+++ static/infoportal/controller/Kiosk.controller.js
@@ -1,22 +1,46 @@
 sap.ui.define([
-  "de/regatta_hd/infopoint/controller/Base.controller"
-], function (BaseController) {
+  "de/regatta_hd/infopoint/controller/Base.controller",
+  "../model/Formatter"
+], function (BaseController, Formatter) {
   "use strict";
 
   return BaseController.extend("de.regatta_hd.infopoint.controller.Statistics", {
 
+    formatter: Formatter,
+
     onInit: function () {
       this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 
-      this.getRouter().getRoute("kiosk").attachMatched(this._loadStatistics, this);
+      this.getRouter().getRoute("kiosk").attachMatched(this._loadKioskModel, this);
     },
 
     onNavBack: function () {
       this.navBack("startpage");
     },
 
-    _loadStatistics: async function () {
+    _loadKioskModel: async function () {
+      if (!this._oKioskModel) {
+        this._oKioskModel = await this.getJSONModel("/api/regattas/" + this.getRegattaId() + "/heats_kiosk", undefined);
+        this.getOwnerComponent().setModel(this._oKioskModel, "heats");
+      } else {
+        await this.updateJSONModel(this._oKioskModel, "/api/regattas/" + this.getRegattaId() + "/heats_kiosk", undefined);
+      }
 
+      const oData = this._oKioskModel.getData();
+
+      if (!this._oFinishedModel) {
+        this._oFinishedModel = await this.getJSONModel("/api/heats/" + oData[0].id + "/registrations", undefined);
+        this.getOwnerComponent().setModel(this._oFinishedModel, "finished");
+      } else {
+        await this.updateJSONModel(this._oFinishedModel, "/api/heats/" + oData[0].id + "/registrations", undefined);
+      }
+
+      if (!this._oNextModel) {
+        this._oNextModel = await this.getJSONModel("/api/heats/" + oData[1].id + "/registrations", undefined);
+        this.getOwnerComponent().setModel(this._oNextModel, "next");
+      } else {
+        await this.updateJSONModel(this._oNextModel, "/api/heats/" + oData[1].id + "/registrations", undefined);
+      }
     }
 
   });
