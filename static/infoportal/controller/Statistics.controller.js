f5639a14eac5e7465181597ffad1ac91aa872e5a
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-03-11 12:32:58 +0100

    label cleanup

--- static/infoportal/controller/Statistics.controller.js
+++ static/infoportal/controller/Statistics.controller.js
@@ -54,7 +54,7 @@ sap.ui.define([
       heats.push({ name: this.i18n("heat.state.official"), value: oStatistics.heats.official });
       heats.push({ name: this.i18n("heat.state.finished"), value: oStatistics.heats.finished });
       heats.push({ name: this.i18n("heat.state.started"), value: oStatistics.heats.started });
-      heats.push({ name: this.i18n("heat.state.seeded"), value: oStatistics.heats.seeded });
+      heats.push({ name: this.i18n("common.seeded"), value: oStatistics.heats.seeded });
       heats.push({ name: this.i18n("common.scheduled"), value: oStatistics.heats.scheduled });
       heats.push({ name: this.i18n("common.cancelled"), value: oStatistics.heats.cancelled });
 
