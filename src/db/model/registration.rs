d1f3ea1e9cea4156d63199755a96a3e7f0d8f3e7
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-05-28 09:02:12 +0200

    don't get club long name

--- src/db/model/registration.rs
+++ src/db/model/registration.rs
@@ -50,7 +50,7 @@ impl ToEntity<Registration> for Row {
 impl Registration {
     pub async fn query_of_club(regatta_id: i32, club_id: i32, client: &mut AquariusClient<'_>) -> Vec<Registration> {
         let mut query = Query::new(
-            "SELECT DISTINCT Entry.*, Label_Short, oc.*, Offer.*
+            "SELECT DISTINCT Entry.*, Label_Short, oc.Club_ID, oc.Club_Abbr, oc.Club_UltraAbbr, oc.Club_City, Offer.*
             FROM Club AS ac
             JOIN Athlet     ON Athlet_Club_ID_FK  = ac.Club_ID
             JOIN Crew       ON Crew_Athlete_ID_FK = Athlet_ID
@@ -68,7 +68,6 @@ impl Registration {
 
         let stream = query.query(client).await.unwrap();
         let rows = utils::get_rows(stream).await;
-        // rows.into_iter().map(|row| row.to_entity()).collect()
 
         let mut registrations: Vec<Registration> = Vec::with_capacity(rows.len());
         for row in &rows {
@@ -82,7 +81,7 @@ impl Registration {
 
     pub async fn query_for_race<'a>(race_id: i32, client: &mut AquariusClient<'_>) -> Vec<Registration> {
         let mut query = Query::new(
-            "SELECT DISTINCT Entry.*, Label_Short, Club.*, Offer.*
+            "SELECT DISTINCT Entry.*, Label_Short, Club.Club_ID, Club.Club_Abbr, Club.Club_UltraAbbr, Club.Club_City, Offer.*
             FROM Entry
             JOIN EntryLabel ON EL_Entry_ID_FK   = Entry_ID
             JOIN Label      ON EL_Label_ID_FK   = Label_ID
