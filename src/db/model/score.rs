0a1002a154d62fdfe7bc7203b9a72c041d861829
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-06-25 16:38:22 +0200

    fixed calc score

--- src/db/model/score.rs
+++ src/db/model/score.rs
@@ -42,26 +42,25 @@ impl Score {
     pub async fn calculate<'a>(regatta_id: i32, client: &mut AquariusClient<'_>) -> Vec<Self> {
         let mut query = Query::new(
             "SELECT Club_ID, SUM(Points_Crew) as points, Club_Name, Club_City, Club_Abbr FROM
-            (
-            SELECT Club_ID, Club_Name, Club_City, Club_Abbr,
-              (SELECT CASE WHEN Offer_HRV_Seeded = 1 AND Comp_HeatNumber = 1
-                THEN
-                  ((RaceMode_LaneCount + 1 - CAST(Result_Rank AS float) + BoatClass_NumRowers) / BoatClass_NumRowers) * 2
-                ELSE 
-                  (RaceMode_LaneCount + 1 - CAST(Result_Rank AS float) + BoatClass_NumRowers) / BoatClass_NumRowers
-                END
-              ) as Points_Crew
+              (SELECT Club_ID, Club_Name, Club_City, Club_Abbr,
+                (SELECT CASE WHEN Offer_HRV_Seeded = 1 AND Comp_HeatNumber = 1
+                  THEN
+                    ((RaceMode_LaneCount + 1 - CAST(Result_Rank AS float) + BoatClass_NumRowers) / BoatClass_NumRowers) * 2
+                  ELSE 
+                    (RaceMode_LaneCount + 1 - CAST(Result_Rank AS float) + BoatClass_NumRowers) / BoatClass_NumRowers
+                  END
+                ) as Points_Crew
               FROM Result
-              JOIN CompEntries ON Result_CE_ID_FK = CE_ID
-              JOIN Comp        ON CE_Comp_ID_FK   = Comp_ID
-              JOIN Entry       ON CE_Entry_ID_FK  = Entry_ID
-              JOIN Crew        ON Crew_Entry_ID_FK = Entry_ID
-              JOIN Athlet      ON Crew_Athlete_ID_FK = Athlet_ID
-              JOIN Club        ON Athlet_Club_ID_FK = Club_ID
-              JOIN Offer       ON Comp_Race_ID_FK = Offer_ID
-              JOIN BoatClass   ON Offer_BoatClass_ID_FK = BoatClass_ID
-              JOIN RaceMode    ON Offer_RaceMode_ID_FK  = RaceMode_ID
-              WHERE Offer_Event_ID_FK = @P1 AND Crew_IsCox = 0 AND Result_SplitNr = 64 AND Crew_RoundTo = 64 AND Result_Rank > 0 AND Comp_Round = 64
+              JOIN CompEntries ON CE_ID        = Result_CE_ID_FK
+              JOIN Comp        ON Comp_ID      = CE_Comp_ID_FK
+              JOIN Entry       ON Entry_ID     = CE_Entry_ID_FK
+              JOIN Crew        ON Entry_ID     = Crew_Entry_ID_FK
+              JOIN Athlet      ON Athlet_ID    = Crew_Athlete_ID_FK
+              JOIN Club        ON Club_ID      = Athlet_Club_ID_FK
+              JOIN Offer       ON Offer_ID     = Comp_Race_ID_FK
+              JOIN BoatClass   ON BoatClass_ID = Offer_BoatClass_ID_FK
+              JOIN RaceMode    ON RaceMode_ID  = Offer_RaceMode_ID_FK
+              WHERE Offer_Event_ID_FK = @P1 AND Crew_IsCox = 0 AND Result_SplitNr = 64 AND Crew_RoundTo = 64 AND Result_Rank > 0 AND Comp_Round = 64 AND Comp_State = 4
             ) as t
             GROUP BY Club_ID, Club_City, Club_Name, Club_Abbr
             ORDER BY points DESC",
