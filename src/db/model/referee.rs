6445952638dc39f4c4f678e04cfd68d0a253a3c7
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-09-26 21:26:56 +0200

    referees

--- src/db/model/referee.rs
+++ src/db/model/referee.rs
@@ -1,16 +1,38 @@
 use crate::db::{
-    model::TryToEntity,
+    aquarius::AquariusClient,
+    model::{utils, ToEntity, TryToEntity},
     tiberius::{RowColumn, TryRowColumn},
 };
 use serde::Serialize;
-use tiberius::Row;
+use tiberius::{Query, Row};
 
-#[derive(Debug, Serialize, Clone, Default)]
+#[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Referee {
     id: i32,
+
+    /// First name of the referee.
     first_name: String,
+
+    /// Last name of the referee.
     last_name: String,
+
+    /// City of the referee.
+    city: String,
+}
+
+impl Referee {
+    pub async fn query<'a>(heat_id: i32, client: &mut AquariusClient<'_>) -> Vec<Referee> {
+        let mut query = Query::new(
+            "SELECT r.* FROM Referee r
+            JOIN CompReferee cr ON cr.CompReferee_Referee_ID_FK = r.Referee_ID
+            WHERE cr.CompReferee_Comp_ID_FK = @P1",
+        );
+        query.bind(heat_id);
+        let stream = query.query(client).await.unwrap();
+        let heats = utils::get_rows(stream).await;
+        heats.into_iter().map(|row| row.to_entity()).collect()
+    }
 }
 
 impl TryToEntity<Referee> for Row {
@@ -21,13 +43,30 @@ impl TryToEntity<Referee> for Row {
             if last_name.is_empty() && first_name.is_empty() {
                 return None;
             }
+            let city: String = self.get_column("Referee_City");
             Some(Referee {
                 id,
                 last_name,
                 first_name,
+                city,
             })
         } else {
             None
+        }
+    }
+}
+
+impl ToEntity<Referee> for Row {
+    fn to_entity(&self) -> Referee {
+        let id = self.get_column("Referee_ID");
+        let last_name: String = self.get_column("Referee_LastName");
+        let first_name: String = self.get_column("Referee_FirstName");
+        let city: String = self.get_column("Referee_City");
+        Referee {
+            id,
+            last_name,
+            first_name,
+            city,
         }
     }
 }
