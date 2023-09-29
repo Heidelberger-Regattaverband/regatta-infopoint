057487576df3a6434452717b45abc42cf0193abc
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-09-29 14:47:37 +0200

    documentation

--- src/db/model/age_class.rs
+++ src/db/model/age_class.rs
@@ -9,10 +9,20 @@ use tiberius::Row;
 #[serde(rename_all = "camelCase")]
 pub struct AgeClass {
     id: i32,
+
+    /// A long and human readable caption of this age class.
     caption: String,
+
+    /// An abbreviation of this age class.
     abbreviation: String,
+
+    /// An suffix to this age class.
     suffix: String,
+
+    /// The gender of this age class. Known values are: "M" (male), "W" (female) or "X" (mixed).
     gender: String,
+
+    /// The number of sub-classes, e.g. for masters age classes
     num_sub_classes: u8,
 }
 
