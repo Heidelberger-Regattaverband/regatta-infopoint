d346cc5be9b120e10256af748345f62e49848e8c
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-09-29 17:26:22 +0200

    filter fixes

--- static/webapp/controller/HeatsTable.controller.js
+++ static/webapp/controller/HeatsTable.controller.js
@@ -34,33 +34,20 @@ sap.ui.define([
               });
               break;
             case 'distance':
-              if (oFilters.distances.length > 1) {
-                oFilters.distances.forEach((distance) => {
-                  oFilterItem.addItem(new ViewSettingsItem({ text: distance + "m", key: "race/distance___EQ___" + distance }));
-                });
-              } else {
-                oFilterItem.setEnabled(false);
-              }
+              oFilters.distances.forEach((distance) => {
+                oFilterItem.addItem(new ViewSettingsItem({ text: distance + "m", key: "race/distance___EQ___" + distance }));
+              });
               break;
             case 'boatClass':
-              if (oFilters.boatClasses.length > 1) {
-                oFilters.boatClasses.forEach((boatClass) => {
-                  oFilterItem.addItem(new ViewSettingsItem({ text: boatClass.caption + " (" + boatClass.abbreviation + ")", key: "race/boatClass/id___EQ___" + boatClass.id }));
-                });
-              } else {
-                oFilterItem.setEnabled(false);
-              }
+              oFilters.boatClasses.forEach((boatClass) => {
+                oFilterItem.addItem(new ViewSettingsItem({ text: boatClass.caption + " (" + boatClass.abbreviation + ")", key: "race/boatClass/id___EQ___" + boatClass.id }));
+              });
               break;
             case 'ageClass':
-              if (oFilters.ageClasses.length > 1) {
-                oFilters.ageClasses.forEach((ageClass) => {
-                  oFilterItem.addItem(new ViewSettingsItem({ text: ageClass.caption + " " + ageClass.suffix + "", key: "race/ageClass/id___EQ___" + ageClass.id }));
-                });
-              } else {
-                oFilterItem.setEnabled(false);
-              }
+              oFilters.ageClasses.forEach((ageClass) => {
+                oFilterItem.addItem(new ViewSettingsItem({ text: ageClass.caption + " " + ageClass.suffix + "", key: "race/ageClass/id___EQ___" + ageClass.id }));
+              });
               break;
-
           } // end switch
         });
       },
