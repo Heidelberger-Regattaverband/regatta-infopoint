360129fd7fbb77057304822d7059de86889202fd
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-10-09 10:24:56 +0200

    added rounds to filters

--- src/db/model/filters.rs
+++ src/db/model/filters.rs
@@ -21,6 +21,19 @@ pub struct Filters {
 
     /// All boat classes used in races
     boat_classes: Vec<BoatClass>,
+
+    /// All rounds: 4 - Vorlauf, 16 - Hoffnungslauf, 32 - Semifinal, 64 - Final
+    rounds: Vec<Round>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+struct Round {
+    /// the round id: 4 - Vorlauf, 16 - Hoffnungslauf, 32 - Semifinal, 64 - Final
+    id: i16,
+
+    /// The code: V - Vorlauf, H - Hoffnungslauf
+    code: String,
 }
 
 impl Filters {
@@ -32,24 +45,26 @@ impl Filters {
         let dates = query_dates(regatta_id, pool);
 
         // get all used age classes
-        let age_classes = query_age_classes(pool, regatta_id);
+        let age_classes = query_age_classes(regatta_id, pool);
 
         // get all used boat classes
-        let boat_classes = query_boat_classes(pool, regatta_id);
+        let boat_classes = query_boat_classes(regatta_id, pool);
 
-        let result = join!(distances, dates, age_classes, boat_classes);
+        let rounds = query_rounds(regatta_id, pool);
+
+        let result = join!(distances, dates, age_classes, boat_classes, rounds);
 
         Filters {
             distances: result.0,
             dates: result.1,
             age_classes: result.2,
             boat_classes: result.3,
+            rounds: result.4,
         }
     }
 }
 
-async fn query_boat_classes(pool: &TiberiusPool, regatta_id: i32) -> Vec<BoatClass> {
-    let mut client = pool.get().await;
+async fn query_boat_classes(regatta_id: i32, pool: &TiberiusPool) -> Vec<BoatClass> {
     let mut query = Query::new(
         "SELECT DISTINCT".to_string()
             + &BoatClass::select_columns("b")
@@ -58,13 +73,13 @@ async fn query_boat_classes(pool: &TiberiusPool, regatta_id: i32) -> Vec<BoatCla
             WHERE o.Offer_Event_ID_FK = @P1 ORDER BY b.BoatClass_NumRowers ASC, b.BoatClass_Coxed ASC",
     );
     query.bind(regatta_id);
-    let stream = query.query(&mut client).await.unwrap();
-    let rows = utils::get_rows(stream).await;
+
+    let mut client = pool.get().await;
+    let rows = utils::get_rows(query.query(&mut client).await.unwrap()).await;
     rows.into_iter().map(|row| row.to_entity()).collect()
 }
 
-async fn query_age_classes(pool: &TiberiusPool, regatta_id: i32) -> Vec<AgeClass> {
-    let mut client = pool.get().await;
+async fn query_age_classes(regatta_id: i32, pool: &TiberiusPool) -> Vec<AgeClass> {
     let mut query = Query::new(
         "SELECT DISTINCT".to_string()
             + &AgeClass::select_columns("a")
@@ -74,27 +89,47 @@ async fn query_age_classes(pool: &TiberiusPool, regatta_id: i32) -> Vec<AgeClass
             ORDER BY a.AgeClass_MinAge DESC, a.AgeClass_MaxAge DESC",
     );
     query.bind(regatta_id);
-    let stream = query.query(&mut client).await.unwrap();
-    let rows = utils::get_rows(stream).await;
+
+    let mut client = pool.get().await;
+    let rows = utils::get_rows(query.query(&mut client).await.unwrap()).await;
     rows.into_iter().map(|row| row.to_entity()).collect()
 }
 
 async fn query_dates(regatta_id: i32, pool: &TiberiusPool) -> Vec<NaiveDate> {
-    let mut client = pool.get().await;
     let mut query = Query::new(
         "SELECT DISTINCT CAST (c.Comp_Datetime as date) AS Comp_Date FROM Comp c WHERE c.Comp_Event_ID_FK = @P1",
     );
     query.bind(regatta_id);
-    let stream = query.query(&mut client).await.unwrap();
-    let rows = utils::get_rows(stream).await;
+
+    let mut client = pool.get().await;
+    let rows = utils::get_rows(query.query(&mut client).await.unwrap()).await;
     rows.into_iter().map(|row| row.get_column("Comp_Date")).collect()
 }
 
 async fn query_distances(regatta_id: i32, pool: &TiberiusPool) -> Vec<i16> {
-    let mut client = pool.get().await;
     let mut query: Query<'_> = Query::new("SELECT DISTINCT Offer_Distance FROM Offer WHERE Offer_Event_ID_FK = @P1");
     query.bind(regatta_id);
-    let stream = query.query(&mut client).await.unwrap();
-    let rows = utils::get_rows(stream).await;
+
+    let mut client = pool.get().await;
+    let rows = utils::get_rows(query.query(&mut client).await.unwrap()).await;
     rows.into_iter().map(|row| row.get_column("Offer_Distance")).collect()
+}
+
+async fn query_rounds(regatta_id: i32, pool: &TiberiusPool) -> Vec<Round> {
+    let mut query: Query<'_> = Query::new(
+        "SELECT DISTINCT
+        c.Comp_Round, c.Comp_RoundCode
+        FROM Comp c WHERE c.Comp_Event_ID_FK = @P1
+        ORDER BY c.Comp_Round ASC",
+    );
+    query.bind(regatta_id);
+
+    let mut client = pool.get().await;
+    let rows = utils::get_rows(query.query(&mut client).await.unwrap()).await;
+    rows.into_iter()
+        .map(|row| Round {
+            id: row.get_column("Comp_Round"),
+            code: row.get_column("Comp_RoundCode"),
+        })
+        .collect()
 }
