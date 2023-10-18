c143c04719d63694ba18ef77c9d4e28eedca8673
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-10-18 20:51:04 +0200

    build urls

--- src/http/crawler.rs
+++ src/http/crawler.rs
@@ -29,9 +29,19 @@ impl Crawler {
     fn parse(&self) {
         let document = Html::parse_document(self.body.as_ref().unwrap());
         let selector = Selector::parse(r#"a"#).unwrap();
-        for title in document.select(&selector) {
-            if let Some(href) = title.value().attr("href") {
-                println!("{}", href);
+        let img_selector = Selector::parse(r#"img"#).unwrap();
+
+        for a in document.select(&selector) {
+            if let Some(href) = a.value().attr("href") {
+                if href.starts_with("/clubs/") {
+                    for img in a.select(&img_selector) {
+                        if let Some(src) = img.value().attr("src") {
+                            let club_id = href.split("/").last().unwrap();
+                            let url = "https://verwaltung.rudern.de/".to_owned() + src;
+                            println!("{} - {}", club_id, url);
+                        }
+                    }
+                }
             }
         }
     }
