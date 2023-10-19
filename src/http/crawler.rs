757b5bb3270b7b732c8b957a72ba3c5dc7760455
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-10-19 09:22:15 +0200

    added test case

--- src/http/crawler.rs
+++ src/http/crawler.rs
@@ -1,35 +1,25 @@
-use log::debug;
-use reqwest::Error;
 use scraper::{Html, Selector};
-use std::fs;
+use std::{collections::HashMap, fs};
+
+const BASE_URL: &str = "https://verwaltung.rudern.de";
 
 struct ClubIcon {
     url: String,
-    club_id: i32,
+    club_id: String,
 }
 
 struct Crawler {
-    url: String,
-    body: Option<String>,
 }
 
 impl Crawler {
-    pub fn new(url: String) -> Self {
-        Crawler { url, body: None }
-    }
-
-    async fn fetch(&mut self) -> Result<&Self, Error> {
-        // let body: String = reqwest::get(&self.url).await?.text().await?;
+    fn parse() -> HashMap<String, ClubIcon> {
         let body: String = fs::read_to_string("static/webapp/flags.html").unwrap();
-        debug!("body = {:?}", body);
-        self.body = Some(body.clone());
-        Ok(self)
-    }
 
-    fn parse(&self) {
-        let document = Html::parse_document(self.body.as_ref().unwrap());
+        let document = Html::parse_document(&body);
         let selector = Selector::parse(r#"a"#).unwrap();
         let img_selector = Selector::parse(r#"img"#).unwrap();
+
+        let mut club_flags = HashMap::new();
 
         for a in document.select(&selector) {
             if let Some(href) = a.value().attr("href") {
@@ -37,13 +27,21 @@ impl Crawler {
                     for img in a.select(&img_selector) {
                         if let Some(src) = img.value().attr("src") {
                             let club_id = href.split("/").last().unwrap();
-                            let url = "https://verwaltung.rudern.de/".to_owned() + src;
+                            let url = BASE_URL.to_owned() + src;
                             println!("{} - {}", club_id, url);
+                            club_flags.insert(
+                                club_id.to_owned(),
+                                ClubIcon {
+                                    url,
+                                    club_id: club_id.to_owned(),
+                                },
+                            );
                         }
                     }
                 }
             }
         }
+        club_flags
     }
 }
 
@@ -53,7 +51,6 @@ mod tests {
 
     #[actix_web::test]
     async fn test_crawler() {
-        let mut crawler = Crawler::new("https://verwaltung.rudern.de/flags".to_owned());
-        let _content = crawler.fetch().await.unwrap().parse();
+        let club_flags = Crawler::parse();
     }
 }
