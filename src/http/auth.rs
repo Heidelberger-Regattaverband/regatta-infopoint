ea8c1275d9f71aa1c59358ba3fa7ed9cf2a95dd8
Markus Ofterdinger <markus.ofterdinger@sap.com>
2023-11-30 10:59:44 +0100

    doc

--- src/http/auth.rs
+++ src/http/auth.rs
@@ -5,33 +5,54 @@ use tiberius::Client;
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
 
+/// The credentials struct contains the username and the password of the user.
+/// The credentials are used to authenticate the user.
 #[derive(Deserialize)]
 pub struct Credentials {
+    /// The username of the user.
     username: String,
+    /// The password of the user.
     password: String,
 }
 
+/// The scope enum contains the possible scopes of the user.
+/// The scope is used to determine the permissions of the user.
 #[derive(Serialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub enum Scope {
+    /// The user is a guest.
     #[default]
     Guest,
+    /// The user is authenticated.
     User,
+    /// The user is an administrator.
     Admin,
 }
 
+/// The user struct contains the username and the scope of the user.
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
+    /// The username of the user.
     pub username: String,
+    /// The scope of the user.
     scope: Scope,
 }
 
 impl User {
+    /// Creates a new user with the given username and scope.
+    /// # Arguments
+    /// * `username` - The username of the user.
+    /// * `scope` - The scope of the user.
+    /// # Returns
+    /// * `User` - The user.
     pub fn new(username: String, scope: Scope) -> Self {
         User { username, scope }
     }
 
+    /// Creates a new user with scope guest.
+    /// # Returns
+    /// * `User` - The guest user.
     pub fn new_guest() -> Self {
         User {
             username: String::from("anonymous"),
@@ -39,6 +60,12 @@ impl User {
         }
     }
 
+    /// Authenticates the user with the given credentials.
+    /// # Arguments
+    /// * `credentials` - The credentials of the user.
+    /// # Returns
+    /// * `Ok(User)` - The authenticated user.
+    /// * `Err(HttpResponse)` - The error response.
     pub async fn authenticate(mut credentials: Credentials) -> Result<Self, HttpResponse> {
         credentials.username = credentials.username.trim().to_owned();
 
