cc0cd97607033933f1b8e6089a6637621e4eb958
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-04-04 19:37:39 +0200

    added heartbeat

--- src/http/ws.rs
+++ src/http/ws.rs
@@ -1,4 +1,4 @@
-use actix::{Actor, StreamHandler};
+use actix::{Actor, ActorContext, AsyncContext, StreamHandler};
 use actix_identity::Identity;
 use actix_web::{
     error::ErrorUnauthorized,
@@ -7,32 +7,101 @@ use actix_web::{
     Error, HttpRequest, HttpResponse,
 };
 use actix_web_actors::ws::{start, Message, ProtocolError, WebsocketContext};
+use log::{debug, warn};
+use std::time::{Duration, Instant};
+
+/// How often heartbeat pings are sent
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+
+/// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
 /// Define HTTP actor
-struct MonitoringWs;
+struct WsMonitoring {
+    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
+    /// otherwise we drop connection.
+    hb: Instant,
+}
 
-impl Actor for MonitoringWs {
+impl WsMonitoring {
+    fn new() -> Self {
+        Self { hb: Instant::now() }
+    }
+
+    /// helper method that sends ping to client every 5 seconds (HEARTBEAT_INTERVAL).
+    /// also this method checks heartbeats from client
+    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            // check client heartbeats
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                // heartbeat timed out
+                warn!("Websocket Client heartbeat failed, disconnecting!");
+
+                // stop actor
+                ctx.stop();
+
+                // don't try to send a ping
+                return;
+            }
+
+            ctx.ping(b"");
+        });
+    }
+}
+
+impl Actor for WsMonitoring {
     type Context = WebsocketContext<Self>;
+
+    /// Method is called on actor start. We start the heartbeat process here.
+    fn started(&mut self, ctx: &mut Self::Context) {
+        debug!("Websocket actor started");
+        self.hb(ctx);
+    }
+
+    /// Method is called on actor stop.
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        debug!("Websocket actor stopped");
+    }
 }
 
 /// Handler for ws::Message message
-impl StreamHandler<Result<Message, ProtocolError>> for MonitoringWs {
+impl StreamHandler<Result<Message, ProtocolError>> for WsMonitoring {
+    /// This method is called for every message received from the websocket client
     fn handle(&mut self, msg: Result<Message, ProtocolError>, ctx: &mut Self::Context) {
+        // process websocket messages
+        debug!("WS: {msg:?}");
         match msg {
-            Ok(Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(Message::Ping(msg)) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            Ok(Message::Pong(_)) => {
+                self.hb = Instant::now();
+            }
             Ok(Message::Text(text)) => ctx.text(text),
             Ok(Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
+            Ok(Message::Close(reason)) => {
+                ctx.close(reason);
+                ctx.stop();
+            }
+            _ => ctx.stop(),
         }
     }
 }
 
 #[get("/ws")]
-async fn index(req: HttpRequest, stream: Payload, opt_user: Option<Identity>) -> Result<HttpResponse, Error> {
+async fn index(request: HttpRequest, stream: Payload, opt_user: Option<Identity>) -> Result<HttpResponse, Error> {
     // if opt_user.is_some() {
-    let resp = start(MonitoringWs {}, &req, stream);
-    println!("{:?}", resp);
-    resp
+    let response = start(WsMonitoring::new(), &request, stream);
+
+    if response.is_ok() {
+        debug!("Websocket connection established");
+    } else {
+        debug!("Websocket connection failed");
+    }
+
+    println!("{:?}", response);
+    response
     // } else {
     //     Err(ErrorUnauthorized("Unauthorized"))
     // }
