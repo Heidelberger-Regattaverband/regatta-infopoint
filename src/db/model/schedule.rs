55e2992c7419db3ae5b038a19b4d6bd72b77a5d0
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-05-13 10:17:45 +0200

    compile fixes

--- src/db/model/schedule.rs
+++ src/db/model/schedule.rs
@@ -1,23 +1,28 @@
-use std::time::Instant;
-
-use actix_web::cookie::time::Date;
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 
 #[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
-pub struct Schedule {
-    generated: Instant,
+pub(crate) struct Schedule {
+    /// The date and time when the schedule was generated
+    generated: DateTime<Utc>,
+
+    /// The schedule entries
     entries: Vec<ScheduleEntry>,
 }
 
 #[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
-pub struct ScheduleEntry {
+pub(crate) struct ScheduleEntry {
     /// The race number
     race_number: String,
+
     /// The race short label (e.g. "MM 2x A-K")
     race_short_label: String,
+
+    /// The number of boats in the race
     num_boats: u8,
+
     /// distance in meters
     distance: i32,
 }
