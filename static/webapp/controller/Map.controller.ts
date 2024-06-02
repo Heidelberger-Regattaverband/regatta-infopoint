1d46a7a6648deaeb13a4abc6a35924c17613a78e
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-06-02 14:15:34 +0200

    added leaflet library

--- static/webapp/controller/Map.controller.ts
+++ static/webapp/controller/Map.controller.ts
@@ -1,6 +1,7 @@
 import BaseController from "./Base.controller";
 import { Button$PressEvent } from "sap/m/Button";
 import { Route$MatchedEvent } from "sap/ui/core/routing/Route";
+import { map, latLng, tileLayer, MapOptions } from "leaflet";
 
 /**
  * @namespace de.regatta_hd.infoportal.controller
@@ -26,7 +27,21 @@ export default class MapController extends BaseController {
   }
 
   private async loadStatistics(): Promise<void> {
+    const options: MapOptions = {
+      center: latLng(40.731253, -73.996139),
+      zoom: 12,
+    };
+    
+    const mymap = map('map', options);
+    
+    const key = "YOUR_MAPTILER_API_KEY_HERE";
+    
+    tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
+      tileSize: 512,
+      zoomOffset: -1,
+      minZoom: 1,
+      attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+      crossOrigin: true
+    }).addTo(mymap);
   }
-
-
 }
