93bda6ac92274acffbf8b18d463ec7c7fd892c78
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-06-02 19:11:53 +0200

    embed div tag

--- static/webapp/controller/Map.controller.ts
+++ static/webapp/controller/Map.controller.ts
@@ -1,7 +1,7 @@
 import BaseController from "./Base.controller";
 import { Button$PressEvent } from "sap/m/Button";
 import { Route$MatchedEvent } from "sap/ui/core/routing/Route";
-import { map, latLng, tileLayer, MapOptions } from "leaflet";
+import { map, latLng, tileLayer, MapOptions, Map } from "leaflet";
 
 /**
  * @namespace de.regatta_hd.infoportal.controller
@@ -10,38 +10,34 @@ export default class MapController extends BaseController {
 
   onInit(): void {
     super.getView()?.addStyleClass(super.getContentDensityClass());
-    super.getRouter()?.getRoute("map")?.attachMatched(async (_: Route$MatchedEvent) => await this.loadStatistics(), this);
-    sap.ui.loader.config({ paths: { "osm": "https://www.openlayers.org/api/OpenLayers" } });
-    sap.ui.require(["osm"], () => this.onOsmLoaded());
+    super.getRouter()?.getRoute("map")?.attachMatched((_: Route$MatchedEvent) => this.loadMap(), this);
+    // sap.ui.loader.config({ paths: { "osm": "https://www.openlayers.org/api/OpenLayers" } });
+    // sap.ui.require(["osm"], () => this.onOsmLoaded());
   }
-  onOsmLoaded() {
-    alert("OSM loaded");
-  }
+  // onOsmLoaded() {
+  //   alert("OSM loaded");
+  // }
 
   onNavBack(): void {
     super.navBack("startpage");
   }
 
-  async onRefreshButtonPress(event: Button$PressEvent): Promise<void> {
-    await this.loadStatistics();
-  }
+  onRefreshButtonPress(event: Button$PressEvent): void {
 
-  private async loadStatistics(): Promise<void> {
+  }
+  private loadMap(): void {
+    // debugger;
     const options: MapOptions = {
       center: latLng(40.731253, -73.996139),
       zoom: 12,
     };
-    
-    const mymap = map('map', options);
-    
-    const key = "YOUR_MAPTILER_API_KEY_HERE";
-    
-    tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
-      tileSize: 512,
-      zoomOffset: -1,
-      minZoom: 1,
-      attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-      crossOrigin: true
+
+    const mymap: Map = map("map", options);
+    // const mymap: Map = map("map", options);
+
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mymap);
   }
 }
