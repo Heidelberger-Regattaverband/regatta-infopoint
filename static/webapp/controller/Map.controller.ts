5061099aba794ae77057792ce349b4a3461dbddc
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-06-04 09:48:00 +0200

    use nominatim of OSM

--- static/webapp/controller/Map.controller.ts
+++ static/webapp/controller/Map.controller.ts
@@ -1,30 +1,31 @@
 import BaseController from "./Base.controller";
-import { Button$PressEvent } from "sap/m/Button";
 import { Route$MatchedEvent } from "sap/ui/core/routing/Route";
 import { map, latLng, tileLayer, MapOptions, Map } from "leaflet";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import * as $ from "jquery";
 
 /**
  * @namespace de.regatta_hd.infoportal.controller
  */
 export default class MapController extends BaseController {
 
+  private readonly participatingClubsModel: JSONModel = new JSONModel();
+
   onInit(): void {
     super.getView()?.addStyleClass(super.getContentDensityClass());
-    super.getRouter()?.getRoute("map")?.attachMatched((_: Route$MatchedEvent) => this.loadMap(), this);
-    // sap.ui.loader.config({ paths: { "osm": "https://www.openlayers.org/api/OpenLayers" } });
-    // sap.ui.require(["osm"], () => this.onOsmLoaded());
+    super.getRouter()?.getRoute("map")?.attachMatched((_: Route$MatchedEvent) => {
+      this.loadModel().then(() => this.loadMap());
+    }, this);
   }
-  // onOsmLoaded() {
-  //   alert("OSM loaded");
-  // }
 
   onNavBack(): void {
     super.navBack("startpage");
   }
 
-  onRefreshButtonPress(event: Button$PressEvent): void {
-
+  async loadModel(): Promise<void> {
+    await super.updateJSONModel(this.participatingClubsModel, `/api/regattas/${this.getRegattaId()}/participating_clubs`);
   }
+
   private loadMap(): void {
     // debugger;
     const options: MapOptions = {
@@ -39,5 +40,23 @@ export default class MapController extends BaseController {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mymap);
+
+    const data: any[] = this.participatingClubsModel.getData();
+    data.forEach((club: any) => {
+      $.ajax({
+        type: "GET",
+        url: "https://nominatim.openstreetmap.org/search",
+        contentType: "application/json",
+        data: {
+          format: "json",
+          city: club.city
+        },
+        success: (result: { username: string; }) => {
+        },
+        error: (result: any) => {
+        }
+      });
+
+    });
   }
 }
