73b4d8da2ef3d29c70f7529d3653525758411af4
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-06-07 22:40:07 +0200

    center button

--- static/webapp/controller/Map.controller.ts
+++ static/webapp/controller/Map.controller.ts
@@ -1,8 +1,8 @@
 import BaseController from "./Base.controller";
 import { Route$MatchedEvent } from "sap/ui/core/routing/Route";
-import { map, latLng, tileLayer, MapOptions, Map } from "leaflet";
+import { map, latLng, tileLayer, MapOptions, Map, LatLng } from "leaflet";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import * as $ from "jquery";
+import { Button$PressEvent } from "sap/m/Button";
 
 /**
  * @namespace de.regatta_hd.infoportal.controller
@@ -10,6 +10,8 @@ import * as $ from "jquery";
 export default class MapController extends BaseController {
 
   private readonly participatingClubsModel: JSONModel = new JSONModel();
+  private readonly center: LatLng = latLng(49.4093582, 8.694724);
+  private map: Map | undefined;
 
   onInit(): void {
     super.getView()?.addStyleClass(super.getContentDensityClass());
@@ -22,24 +24,29 @@ export default class MapController extends BaseController {
     super.navBack("startpage");
   }
 
-  async loadModel(): Promise<void> {
+  onCenterButtonPress(_event: Button$PressEvent): void {
+    if (this.map) {
+      this.map.setView(this.center);
+    }
+  }
+
+  private async loadModel(): Promise<void> {
     await super.updateJSONModel(this.participatingClubsModel, `/api/regattas/${this.getRegattaId()}/participating_clubs`);
   }
 
   private loadMap(): void {
     // debugger;
     const options: MapOptions = {
-      center: latLng(49.4093582, 8.694724),
+      center: this.center,
       zoom: 14,
     };
 
-    const mymap: Map = map("map", options);
-    // const mymap: Map = map("map", options);
+    this.map = map("map", options);
 
     tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(mymap);
+    }).addTo(this.map);
 
     const data: any[] = this.participatingClubsModel.getData();
     data.forEach((club: any) => {
