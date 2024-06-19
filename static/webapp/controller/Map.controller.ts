ec497d77ca986d0edb0f102a8412f1aca0f4b9f6
Markus Ofterdinger <markus.ofterdinger@sap.com>
2024-06-19 22:41:47 +0200

    fix ts build

--- static/webapp/controller/Map.controller.ts
+++ static/webapp/controller/Map.controller.ts
@@ -3,7 +3,6 @@ import { Route$MatchedEvent } from "sap/ui/core/routing/Route";
 import { map, latLng, tileLayer, MapOptions, Map, LatLng, marker, popup, LatLngBounds, icon, layerGroup, Marker, TileLayer, LayerGroup, control, latLngBounds, FitBoundsOptions, Control } from "leaflet";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import { Button$PressEvent } from "sap/m/Button";
-import "leaflet/dist/leaflet.css";
 
 /**
  * @namespace de.regatta_hd.infoportal.controller
