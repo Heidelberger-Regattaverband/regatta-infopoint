2544bb917a5634e1764db3b74727e49992027903
Markus Ofterdinger <markus@ofterdinger.de>
2024-08-28 17:00:08 +0200

    added struct Client

--- timekeeper/src/main.rs
+++ timekeeper/src/main.rs
@@ -16,6 +16,36 @@ struct Cli {
     port: String,
 }
 
+struct Client {
+    stream: TcpStream,
+}
+
+impl Client {
+    fn new() -> Self {
+        info!("Parsing command line arguments");
+        let args = Cli::parse();
+
+        info!("Connecting to: {}:{}", args.host, args.port);
+        let stream = TcpStream::connect(format!("{}:{}", args.host, args.port)).unwrap();
+        Client { stream }
+    }
+
+    fn send_cmd(&mut self, cmd: &str) -> Result<()> {
+        info!("Writing command: \"{}\"", cmd);
+        let result = self.stream.write(cmd.as_bytes())?;
+        self.stream.flush().unwrap();
+        info!("Written {} bytes", result);
+        Ok(())
+    }
+
+    fn receive(&mut self) -> Result<String> {
+        info!("Receiving ...");
+        let mut line = String::with_capacity(512);
+        self.stream.read_to_string(&mut line)?;
+        Ok(line)
+    }
+}
+
 fn main() -> Result<()> {
     env_logger::builder()
         .filter_level(LevelFilter::Info)
@@ -23,22 +53,11 @@ fn main() -> Result<()> {
         // .format_timestamp(None)
         .init();
 
-    info!("Parsing command line arguments");
-    let args = Cli::parse();
+    let mut client = Client::new();
+    client.send_cmd("?OPEN\r\n")?;
 
-    info!("Connecting to: {}:{}", args.host, args.port);
-    let mut stream = TcpStream::connect(format!("{}:{}", args.host, args.port))?;
-
-    let cmd = "?OPEN\r\n";
-    info!("Writing command: \"{}\"", cmd);
-    let result = stream.write(cmd.as_bytes())?;
-    info!("Written {} bytes", result);
-
-    info!("Reading response ...");
-    let mut buf = Vec::with_capacity(128);
-    let read = stream.read(&mut buf)?;
-    let response = String::from_utf8(buf).unwrap();
-    info!("Read {} bytes: \"{}\"", read, response);
+    let received = client.receive()?;
+    info!("Received:\"{}\"", received);
 
     Ok(())
 } // the stream is closed here
