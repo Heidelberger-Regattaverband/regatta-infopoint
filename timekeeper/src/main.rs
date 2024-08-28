02f1e5b56586f12472715efb3a2073b12bb85028
Markus Ofterdinger <markus@ofterdinger.de>
2024-08-28 21:31:04 +0200

    renamed Cli to Args

--- timekeeper/src/main.rs
+++ timekeeper/src/main.rs
@@ -9,7 +9,7 @@ use std::{
 #[command(name = "TimeKeeper")]
 #[command(version = "0.1.0")]
 #[command(about = "A Timekeeper for Aquarius", long_about = None)]
-struct Cli {
+struct Args {
     #[arg(long)]
     host: String,
     #[arg(long)]
@@ -23,17 +23,18 @@ struct Client {
 impl Client {
     fn new() -> Self {
         info!("Parsing command line arguments");
-        let args = Cli::parse();
+        let args = Args::parse();
 
-        info!("Connecting to: {}:{}", args.host, args.port);
+        info!("Connecting to {}:{}", args.host, args.port);
         let stream = TcpStream::connect(format!("{}:{}", args.host, args.port)).unwrap();
+        stream.set_nodelay(true).unwrap();
         Client { stream }
     }
 
     fn send_cmd(&mut self, cmd: &str) -> Result<()> {
         info!("Writing command: \"{}\"", cmd);
         let result = self.stream.write(cmd.as_bytes())?;
-        self.stream.flush().unwrap();
+        self.stream.flush()?;
         info!("Written {} bytes", result);
         Ok(())
     }
@@ -47,14 +48,10 @@ impl Client {
 }
 
 fn main() -> Result<()> {
-    env_logger::builder()
-        .filter_level(LevelFilter::Info)
-        // .format_target(false)
-        // .format_timestamp(None)
-        .init();
+    env_logger::builder().filter_level(LevelFilter::Info).init();
 
     let mut client = Client::new();
-    client.send_cmd("?OPEN\r\n")?;
+    client.send_cmd("?STARTLIST nr=50\r\n")?;
 
     let received = client.receive()?;
     info!("Received:\"{}\"", received);
