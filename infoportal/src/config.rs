eb0c9b0e5cbb997e04594de80f17c3fd93f07704
Markus Ofterdinger <markus@ofterdinger.de>
2024-09-20 17:31:25 +0200

    stability

--- infoportal/src/config.rs
+++ infoportal/src/config.rs
@@ -131,7 +131,7 @@ impl Config {
         info!(
             "Build: time {}, commit {}, head_ref {}, ",
             built_info::BUILT_TIME_UTC.bold(),
-            built_info::GIT_COMMIT_HASH.unwrap().bold(),
+            built_info::GIT_COMMIT_HASH.unwrap_or_default().bold(),
             built_info::GIT_HEAD_REF.unwrap_or_default().bold()
         );
 
