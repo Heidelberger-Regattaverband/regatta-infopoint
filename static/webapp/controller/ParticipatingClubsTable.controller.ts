8d20a967d3cd0267a0d5c9dbfd5dd59d58c21e28
Markus Ofterdinger <markus@ofterdinger.de>
2024-10-13 10:08:51 +0200

    fixed wrong navigation in map

--- static/webapp/controller/ParticipatingClubsTable.controller.ts
+++ static/webapp/controller/ParticipatingClubsTable.controller.ts
@@ -28,7 +28,7 @@ export default class ParticipatingClubsTable extends BaseTableController {
   }
 
   onNavBack(): void {
-    super.navBack("startpage");
+    super.navToStartPage();
   }
 
   onSearchFieldLiveChange(event: SearchField$LiveChangeEvent): void {
