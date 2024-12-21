c339e747f696468028958a80dfb3fb6df713bc38
Markus Ofterdinger <markus@ofterdinger.de>
2024-12-21 18:13:36 +0100

    Timekeeper (#204)
    * Initial version of timekeeper project
    
    * logging
    
    * added struct Client
    
    * renamed Cli to Args
    
    * npm update
    
    * Cargo.toml cleanup
    
    * use buffered reader and writer
    
    * cleanup
    
    * use thread to read from stream
    
    * added client.rs
    
    * error message
    
    * debug traces
    
    * improvements
    
    * traces
    
    * trim
    
    * npm update
    
    * cargo update

---
+++ timekeeper/src/main.rs
@@ -0,0 +1,37 @@
+mod client;
+
+use clap::{command, Parser};
+use client::Client;
+use log::{info, LevelFilter};
+use std::{io::Result, thread};
+
+#[derive(Parser)]
+#[command(name = "TimeKeeper")]
+#[command(version = "0.1.0")]
+#[command(about = "A Timekeeper for Aquarius", long_about = None)]
+struct Args {
+    #[arg(long)]
+    host: String,
+    #[arg(long)]
+    port: String,
+}
+
+fn main() -> Result<()> {
+    env_logger::builder().filter_level(LevelFilter::Info).init();
+    let args = Args::parse();
+
+    let mut client = Client::new(args.host, args.port)?;
+    client.write("?OPEN\n")?;
+
+    info!("Receiving ...");
+    thread::spawn(move || loop {
+        let received = client.receive().unwrap();
+        if !received.is_empty() {
+            info!("Received: \"{}\"", received);
+        }
+    })
+    .join()
+    .unwrap();
+
+    Ok(())
+} // the stream is closed here
