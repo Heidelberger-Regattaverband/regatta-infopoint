7ed6389be4d14c0b8bd167f0c44eefa1962f8465
Markus Ofterdinger <markus@ofterdinger.de>
2024-12-30 14:22:03 +0100

    added tests

--- timekeeper/src/messages.rs
+++ timekeeper/src/messages.rs
@@ -57,3 +57,38 @@ impl Heat {
         Heat { id, number, status }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_request_list_open_heats() {
+        let request = RequestListOpenHeats::new();
+        assert_eq!(request.to_string(), "?OPEN\n");
+    }
+
+    #[test]
+    fn test_response_list_open_heats() {
+        let message = "3 2766 4\n50 2767 4\n71 2786 4";
+        let response = ResponseListOpenHeats::new(message);
+        assert_eq!(response.heats.len(), 3);
+        assert_eq!(response.heats[0].id, 2766);
+        assert_eq!(response.heats[0].number, 3);
+        assert_eq!(response.heats[0].status, 4);
+        assert_eq!(response.heats[1].id, 2767);
+        assert_eq!(response.heats[1].number, 50);
+        assert_eq!(response.heats[1].status, 4);
+        assert_eq!(response.heats[2].id, 2786);
+        assert_eq!(response.heats[2].number, 71);
+        assert_eq!(response.heats[2].status, 4);
+    }
+
+    #[test]
+    fn test_heat() {
+        let heat = Heat::new(1, 1, 4);
+        assert_eq!(heat.number, 1);
+        assert_eq!(heat.id, 1);
+        assert_eq!(heat.status, 4);
+    }
+}
