6240454304587ad27b654f5dd33b64982cdcd134
Markus Ofterdinger <markus@ofterdinger.de>
2025-01-04 11:09:12 +0100

    refactoring

--- timekeeper/src/main.rs
+++ timekeeper/src/main.rs
@@ -18,7 +18,7 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     let mut client = Client::new(args.host, args.port)?;
-    let open_heats = get_open_heats(&mut client)?;
+    let open_heats = read_open_heats(&mut client)?;
     debug!("Open heats: {:#?}", open_heats);
 
     info!("Receiving events ...");
@@ -26,7 +26,10 @@ fn main() -> Result<()> {
         let received = client.receive_line().unwrap();
         if !received.is_empty() {
             debug!("Received: \"{}\"", utils::print_whitespaces(&received).bold());
-            parse_event(&received);
+            let event_opt = parse_event(&received);
+            if let Some(mut event) = event_opt {
+                read_start_list(&mut client, &mut event.heat).unwrap();
+            }
         }
     })
     .join()
@@ -81,17 +84,22 @@ fn parse_event(event: &str) -> Option<EventHeatChanged> {
     }
 }
 
-fn get_open_heats(client: &mut Client) -> Result<Vec<Heat>> {
+fn read_open_heats(client: &mut Client) -> Result<Vec<Heat>> {
     client.write(&RequestListOpenHeats::new().to_string())?;
     let response = client.receive_all()?;
     let mut open_heats = ResponseListOpenHeats::parse(&response);
 
     for heat in open_heats.heats.iter_mut() {
-        client.write(&RequestStartList::new(heat.id).to_string())?;
-        let response = client.receive_all()?;
-        let start_list = ResponseStartList::parse(response);
-        heat.boats = Some(start_list.boats);
+        read_start_list(client, heat)?;
     }
 
     Ok(open_heats.heats)
+}
+
+fn read_start_list(client: &mut Client, heat: &mut Heat) -> Result<()> {
+    client.write(&RequestStartList::new(heat.id).to_string())?;
+    let response = client.receive_all()?;
+    let start_list = ResponseStartList::parse(response);
+    heat.boats = Some(start_list.boats);
+    Ok(())
 }
