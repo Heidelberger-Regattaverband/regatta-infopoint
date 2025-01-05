8c15163f25481bc6866c381d2785cd45a207a978
Markus Ofterdinger <markus@ofterdinger.de>
2025-01-05 19:55:58 +0100

    Moved event listening into Client (#208)
    * Add stream to struct Client
    
    * improved logging

--- timekeeper/src/client.rs
+++ timekeeper/src/client.rs
@@ -1,14 +1,17 @@
 use crate::{
     error::MessageErr,
-    messages::{Heat, RequestListOpenHeats, RequestStartList, ResponseListOpenHeats, ResponseStartList},
+    messages::{
+        EventHeatChanged, Heat, RequestListOpenHeats, RequestStartList, ResponseListOpenHeats, ResponseStartList,
+    },
     utils,
 };
 use colored::Colorize;
-use log::{debug, info, trace};
+use log::{debug, info, trace, warn};
 use std::{
     io::{BufRead, BufReader, BufWriter, Result as IoResult, Write},
     net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream},
     str::FromStr,
+    thread::{self, JoinHandle},
     time::Duration,
 };
 
@@ -19,6 +22,9 @@ pub(crate) struct Client {
 
     /// A buffered writer to write to the server.
     writer: BufWriter<TcpStream>,
+
+    /// The TCP stream to the server.
+    stream: TcpStream,
 }
 
 impl Client {
@@ -35,46 +41,72 @@ impl Client {
         info!("Connecting to {}", addr.to_string().bold());
         let stream = TcpStream::connect_timeout(&addr, Duration::new(timeout as u64, 0))?;
         stream.set_nodelay(true)?;
-        let write_stream = stream.try_clone()?;
-        let reader = BufReader::new(stream);
-        let writer = BufWriter::new(write_stream);
+        // let write_stream = stream.try_clone()?;
+        let reader = BufReader::new(stream.try_clone()?);
+        let writer = BufWriter::new(stream.try_clone()?);
         info!("Connected to {}", addr.to_string().bold());
-        Ok(Client { reader, writer })
+        Ok(Client { reader, writer, stream })
+    }
+
+    pub(crate) fn start_receiving_events(&mut self) -> IoResult<JoinHandle<()>> {
+        let mut reader = BufReader::new(self.stream.try_clone()?);
+        let mut writer = BufWriter::new(self.stream.try_clone()?);
+
+        debug!("Starting thread to receive events");
+        let handle = thread::spawn(move || loop {
+            debug!("Waiting for events");
+            let received = Client::receive_line(&mut reader).unwrap();
+            if !received.is_empty() {
+                debug!("Received: \"{}\"", utils::print_whitespaces(&received).bold());
+                let event_opt = EventHeatChanged::parse(&received);
+                match event_opt {
+                    Ok(mut event) => {
+                        if event.opened {
+                            Client::read_start_list(&mut reader, &mut writer, &mut event.heat).unwrap();
+                        }
+                    }
+                    Err(err) => handle_error(err),
+                }
+            }
+        });
+        Ok(handle)
     }
 
     pub(crate) fn read_open_heats(&mut self) -> Result<Vec<Heat>, MessageErr> {
-        self.write(&RequestListOpenHeats::new().to_string())
-            .map_err(MessageErr::IoError)?;
-        let response = self.receive_all().map_err(MessageErr::IoError)?;
-        let mut open_heats = ResponseListOpenHeats::parse(&response).unwrap();
+        Client::write(&mut self.writer, &RequestListOpenHeats::new().to_string()).map_err(MessageErr::IoError)?;
+        let response = Client::receive_all(&mut self.reader).map_err(MessageErr::IoError)?;
+        let mut open_heats = ResponseListOpenHeats::parse(&response)?;
 
         for heat in open_heats.heats.iter_mut() {
-            self.read_start_list(heat)?;
+            Client::read_start_list(&mut self.reader, &mut self.writer, heat)?;
         }
 
         Ok(open_heats.heats)
     }
 
-    pub(crate) fn read_start_list(&mut self, heat: &mut Heat) -> Result<(), MessageErr> {
-        self.write(&RequestStartList::new(heat.id).to_string())
-            .map_err(MessageErr::IoError)?;
-        let response = self.receive_all().map_err(MessageErr::IoError)?;
+    fn read_start_list(
+        reader: &mut BufReader<TcpStream>,
+        writer: &mut BufWriter<TcpStream>,
+        heat: &mut Heat,
+    ) -> Result<(), MessageErr> {
+        Client::write(writer, &RequestStartList::new(heat.id).to_string()).map_err(MessageErr::IoError)?;
+        let response = Client::receive_all(reader).map_err(MessageErr::IoError)?;
         let start_list = ResponseStartList::parse(response)?;
         heat.boats = Some(start_list.boats);
         Ok(())
     }
 
-    fn write(&mut self, cmd: &str) -> IoResult<usize> {
+    fn write(writer: &mut BufWriter<TcpStream>, cmd: &str) -> IoResult<usize> {
         debug!("Writing command: \"{}\"", utils::print_whitespaces(cmd).bold());
-        let count = self.writer.write(cmd.as_bytes())?;
-        self.writer.flush()?;
+        let count = writer.write(cmd.as_bytes())?;
+        writer.flush()?;
         trace!("Written {} bytes", count.to_string().bold());
         Ok(count)
     }
 
-    pub(crate) fn receive_line(&mut self) -> IoResult<String> {
+    fn receive_line(reader: &mut BufReader<TcpStream>) -> IoResult<String> {
         let mut line = String::new();
-        let count = self.reader.read_line(&mut line)?;
+        let count = reader.read_line(&mut line)?;
         trace!(
             "Received {} bytes: \"{}\"",
             count.to_string().bold(),
@@ -83,12 +115,12 @@ impl Client {
         Ok(line.trim_end().to_string())
     }
 
-    fn receive_all(&mut self) -> IoResult<String> {
+    fn receive_all(reader: &mut BufReader<TcpStream>) -> IoResult<String> {
         let mut result = String::new();
         let mut buf = Vec::new();
         loop {
             // Read until a newline character is found.
-            let count = self.reader.read_until(b'\n', &mut buf)?;
+            let count = reader.read_until(b'\n', &mut buf)?;
             // Convert the buffer to a string, ignoring invalid UTF-8 sequences.
             let line = String::from_utf8_lossy(&buf);
             trace!(
@@ -109,6 +141,20 @@ impl Client {
             utils::print_whitespaces(&result).bold()
         );
         Ok(result.trim_end().to_string())
+    }
+}
+
+fn handle_error(err: MessageErr) {
+    match err {
+        MessageErr::ParseError(parse_err) => {
+            warn!("Error parsing number: {}", parse_err);
+        }
+        MessageErr::IoError(io_err) => {
+            warn!("I/O error: {}", io_err);
+        }
+        MessageErr::InvalidMessage(message) => {
+            warn!("Invalid message: {}", message);
+        }
     }
 }
 
@@ -164,7 +210,7 @@ mod tests {
         let addr = start_test_server();
         let mut client = Client::new(addr.ip().to_string(), addr.port(), 1).unwrap();
         const MESSAGE: &str = "Hello World!";
-        let result = client.write(MESSAGE);
+        let result = Client::write(&mut client.writer, MESSAGE);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), MESSAGE.len());
     }
@@ -176,9 +222,9 @@ mod tests {
         let addr = start_test_server();
         let mut client = Client::new(addr.ip().to_string(), addr.port(), 1).unwrap();
         const MESSAGE: &str = "Hello World!";
-        client.write(MESSAGE).unwrap();
-        client.write("\r\n").unwrap();
-        let response = client.receive_line();
+        Client::write(&mut client.writer, MESSAGE).unwrap();
+        Client::write(&mut client.writer, "\r\n").unwrap();
+        let response = Client::receive_line(&mut client.reader);
         assert!(response.is_ok());
         assert_eq!(response.unwrap(), MESSAGE);
     }
@@ -187,10 +233,10 @@ mod tests {
     fn test_client_receive_all() {
         let addr = start_test_server();
         let mut client = Client::new(addr.ip().to_string(), addr.port(), 1).unwrap();
-        client.write("Hello World!\n").unwrap();
-        client.write("This is a test.\n").unwrap();
-        client.write("\r\n").unwrap();
-        let response = client.receive_all();
+        Client::write(&mut client.writer, "Hello World!\n").unwrap();
+        Client::write(&mut client.writer, "This is a test.\n").unwrap();
+        Client::write(&mut client.writer, "\r\n").unwrap();
+        let response = Client::receive_all(&mut client.reader);
         assert!(response.is_ok());
         assert_eq!(response.unwrap(), "Hello World!\nThis is a test.");
     }
