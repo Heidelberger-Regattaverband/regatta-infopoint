b8d80a0bf5c5d12a04b883f55e1c0a6e7476c85a
Markus Ofterdinger <markus@ofterdinger.de>
2025-01-06 20:12:50 +0100

    added build.rs

--- timekeeper/src/args.rs
+++ timekeeper/src/args.rs
@@ -1,14 +1,23 @@
 use clap::{command, Parser};
 
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
 #[derive(Parser)]
-#[command(name = "TimeKeeper")]
-#[command(version = "0.1.0")]
-#[command(about = "A Timekeeper for Aquarius", long_about = None)]
+#[command(name = built_info::PKG_NAME)]
+#[command(version = built_info::PKG_VERSION)]
+#[command(about = built_info::PKG_DESCRIPTION, long_about = None)]
 pub(crate) struct Args {
+    /// The host to connect to
     #[arg(long, default_value = "localhost")]
     pub(crate) host: String,
+
+    /// The port to connect to
     #[arg(long, default_value = "2048")]
     pub(crate) port: u16,
+
+    /// The connection timeout in seconds
     #[arg(long, default_value = "2")]
     pub(crate) timeout: u16,
 }
