6d826b14d6770b3ce39d606403c0b2d6fae60a24
Markus Ofterdinger <markus@ofterdinger.de>
2025-09-05 19:49:30 +0200

    TuiWidgetEvent

--- timekeeper/src/app/logs_tab.rs
+++ timekeeper/src/app/logs_tab.rs
@@ -1,13 +1,35 @@
 use crate::app::utils::block;
-use ratatui::{buffer::Buffer, crossterm::event::KeyEvent, layout::Rect, widgets::Widget};
-use tui_logger::{TuiLoggerLevelOutput, TuiLoggerWidget};
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{KeyCode, KeyEvent},
+    layout::Rect,
+    widgets::Widget,
+};
+use tui_logger::{TuiLoggerLevelOutput, TuiLoggerWidget, TuiWidgetEvent, TuiWidgetState};
 
 #[derive(Default)]
-pub(crate) struct LogsTab {}
+pub(crate) struct LogsTab {
+    state: TuiWidgetState,
+}
 
 impl LogsTab {
-    pub(crate) fn handle_key_event(&mut self, _event: KeyEvent) {
-        // Currently no key events are handled in the logs tab
+    pub(crate) fn handle_key_event(&mut self, event: KeyEvent) {
+        let tui_event = match event.code {
+            KeyCode::Up => TuiWidgetEvent::UpKey,
+            KeyCode::Down => TuiWidgetEvent::DownKey,
+            KeyCode::Left => TuiWidgetEvent::LeftKey,
+            KeyCode::Right => TuiWidgetEvent::RightKey,
+            KeyCode::PageUp => TuiWidgetEvent::PrevPageKey,
+            KeyCode::PageDown => TuiWidgetEvent::NextPageKey,
+            KeyCode::Esc => TuiWidgetEvent::EscapeKey,
+            KeyCode::Char(' ') => TuiWidgetEvent::SpaceKey,
+            KeyCode::Char('+') => TuiWidgetEvent::PlusKey,
+            KeyCode::Char('-') => TuiWidgetEvent::MinusKey,
+            KeyCode::Char('h') => TuiWidgetEvent::HideKey,
+            _ => return,
+        };
+
+        self.state.transition(tui_event);
     }
 }
 
@@ -21,6 +43,7 @@ impl Widget for &mut LogsTab {
             .output_target(true)
             .output_file(false)
             .output_line(false)
+            .state(&self.state)
             .render(area, buf);
     }
 }
