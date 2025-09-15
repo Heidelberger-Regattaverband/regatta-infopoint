70686e912be248ee51a85c6aa660c0174a411e23
Markus Ofterdinger <markus@ofterdinger.de>
2025-09-15 10:20:19 +0200

    close popup

--- timekeeper/src/app/timestrip_popup.rs
+++ timekeeper/src/app/timestrip_popup.rs
@@ -14,7 +14,7 @@ use tui_prompts::prelude::*;
 pub(crate) struct TimeStripTabPopup<'a> {
     heat_state: TextState<'a>,
     bib_state: TextState<'a>,
-    is_valid: bool,
+    current_field: Field,
 
     // shared context
     client: Rc<RefCell<Client>>,
@@ -34,18 +34,18 @@ impl Widget for &mut TimeStripTabPopup<'_> {
             .title(format!(" {ts_split} "));
 
         // inner popup area
-        let inner_area = block.inner(area);
+        let [heat_area, bib_area] =
+            Layout::vertical([Constraint::Length(1), Constraint::Length(1)]).areas(block.inner(area));
         block.render(area, buffer);
 
-        let [heat_area, bib_area] = Layout::vertical([Constraint::Length(1), Constraint::Length(1)]).areas(inner_area);
-        TextPrompt::from("Lauf").render(heat_area, buffer, &mut self.heat_state);
+        TextPrompt::from("Lauf    ").render(heat_area, buffer, &mut self.heat_state);
         if matches!(ts_split, Split::Finish) {
             TextPrompt::from("Startnr.").render(bib_area, buffer, &mut self.bib_state);
         }
     }
 }
 
-impl TimeStripTabPopup<'_> {
+impl<'a> TimeStripTabPopup<'a> {
     pub(crate) fn new(
         client: Rc<RefCell<Client>>,
         heats: Rc<RefCell<Vec<Heat>>>,
@@ -56,7 +56,7 @@ impl TimeStripTabPopup<'_> {
         Self {
             heat_state: TextState::default(),
             bib_state: TextState::default(),
-            is_valid: false,
+            current_field: Field::default(),
             client,
             heats,
             time_strip,
@@ -67,44 +67,99 @@ impl TimeStripTabPopup<'_> {
 
     #[allow(clippy::await_holding_refcell_ref)]
     pub(crate) async fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Esc => {
-                if self.heat_state.is_empty() {
-                    *self.show_time_strip_popup.borrow_mut() = false;
-                } else {
-                    self.heat_state.delete();
-                }
+        match (key_event.code, key_event.modifiers) {
+            (KeyCode::Esc, _) => {
+                // close popup without saving
+                *self.show_time_strip_popup.borrow_mut() = false;
             }
-            KeyCode::Enter => {
-                if self.is_valid {
-                    let heat_nr = self.heat_state.value().parse::<i16>().unwrap();
-                    self.heat_state.delete();
-                    if let Some(time_stamp) = self.selected_time_stamp.borrow().as_ref()
-                        && let Ok(time_stamp) = self.time_strip.borrow_mut().set_heat_nr(time_stamp, heat_nr).await
-                    {
-                        *self.show_time_strip_popup.borrow_mut() = false;
-                        self.client.borrow_mut().send_time(&time_stamp, None).unwrap();
-                    }
-                    self.is_valid = false;
-                }
+            (KeyCode::Enter, _) => {
+                self.submit();
+                // if input is finished close popup
+                *self.show_time_strip_popup.borrow_mut() = !self.is_finished();
+                // if self.is_valid {
+                //     let heat_nr = self.heat_state.value().parse::<i16>().unwrap();
+                //     self.heat_state.delete();
+                //     if let Some(time_stamp) = self.selected_time_stamp.borrow().as_ref()
+                //         && let Ok(time_stamp) = self.time_strip.borrow_mut().set_heat_nr(time_stamp, heat_nr).await
+                //     {
+                //         *self.show_time_strip_popup.borrow_mut() = false;
+                //         self.client.borrow_mut().send_time(&time_stamp, None).unwrap();
+                //     }
+                //     self.is_valid = false;
+                // }
             }
             _ => {
-                self.heat_state.handle_key_event(key_event);
-                self.validate();
+                self.current_state().handle_key_event(key_event);
+                // self.validate();
             }
         }
     }
 
-    fn validate(&mut self) {
-        if let Ok(heat_nr) = self.heat_state.value().parse::<i16>() {
-            self.is_valid = self.heats.borrow().iter().any(|heat| heat.number == heat_nr);
-        } else {
-            self.is_valid = false;
+    fn validate(&mut self) -> bool {
+        match self.current_field {
+            Field::Heat => self.validate_heat(),
+            Field::Bib => self.validate_bib(),
         }
         // if self.is_valid {
-        //     self.heat_state.set_style(Style::default().fg(Color::LightGreen));
+        //     self.heat_state.sta set_style(Style::default().fg(Color::LightGreen));
         // } else {
         //     self.heat_state.set_style(Style::default().fg(Color::LightRed));
         // }
     }
+
+    fn validate_heat(&self) -> bool {
+        if let Ok(heat_nr) = self.heat_state.value().parse::<i16>() {
+            self.heats.borrow().iter().any(|heat| heat.number == heat_nr)
+        } else {
+            false
+        }
+    }
+    fn validate_bib(&self) -> bool {
+        if let (Ok(heat_nr), Ok(bib_nr)) = (
+            self.heat_state.value().parse::<i16>(),
+            self.bib_state.value().parse::<u8>(),
+        ) {
+            self.heats.borrow().iter().any(|heat| {
+                heat.number == heat_nr && heat.boats.as_ref().unwrap().iter().any(|boat| boat.bib == bib_nr)
+            })
+        } else {
+            false
+        }
+    }
+
+    fn submit(&mut self) {
+        if self.validate() {
+            self.current_state().complete();
+            if self.current_state().is_finished() && !self.is_finished() {
+                self.focus_next();
+            }
+        }
+    }
+    fn focus_next(&mut self) {
+        self.current_state().blur();
+        self.current_field = self.next_field();
+        self.current_state().focus();
+    }
+    const fn next_field(&self) -> Field {
+        match self.current_field {
+            Field::Heat => Field::Bib,
+            Field::Bib => Field::Heat,
+        }
+    }
+    const fn current_state(&mut self) -> &mut TextState<'a> {
+        match self.current_field {
+            Field::Heat => &mut self.heat_state,
+            Field::Bib => &mut self.bib_state,
+        }
+    }
+    const fn is_finished(&self) -> bool {
+        self.heat_state.is_finished() && self.bib_state.is_finished()
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+enum Field {
+    #[default]
+    Heat,
+    Bib,
 }
