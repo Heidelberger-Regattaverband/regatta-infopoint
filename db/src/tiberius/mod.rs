7a73e117950c5dde04a6565abc0f45e9eb1ad40c
Markus Ofterdinger <markus@ofterdinger.de>
2025-10-11 13:40:25 +0200

    fix: expose TiberiusConnection for easier access in the module

--- db/src/tiberius/mod.rs
+++ db/src/tiberius/mod.rs
@@ -2,6 +2,7 @@ mod connection;
 mod pool;
 mod row_column;
 
+pub use connection::TiberiusConnection;
 pub use connection::TiberiusConnectionManager;
 pub use pool::TiberiusPool;
 pub use row_column::RowColumn;
