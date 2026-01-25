fc27a1afd94054b2edf862a6269997bdbd30b81c
Markus Ofterdinger <markus@ofterdinger.de>
2026-01-25 21:25:05 +0100

    fix: add missing get_all_notifications endpoint to API documentation

--- infoportal/src/http/api_doc.rs
+++ infoportal/src/http/api_doc.rs
@@ -26,6 +26,7 @@ use ::utoipa_swagger_ui::SwaggerUi;
         rest_api::misc::calculate_scoring,
         rest_api::misc::get_schedule,
         rest_api::notification::get_notifications,
+        rest_api::notification::get_all_notifications,
         rest_api::notification::create_notification,
         rest_api::notification::update_notification,
         rest_api::notification::delete_notification,
